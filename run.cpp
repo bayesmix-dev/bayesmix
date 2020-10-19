5e26014accca7c00bcf4341c6a767593b3873470
Bruno Guindani <bruno@guindani.org>
2020-10-19 21:04:51 +0200

    Added run.cpp

---
+++ run.cpp
@@ -0,0 +1,16 @@
+#include <math.h>
+
+#include <fstream>
+#include <iostream>
+
+#include "src/includes.hpp"
+
+int main(int argc, char *argv[]) {
+  // [0]main [1]data [2]algo [3]coll [4]filecollname
+  std::cout << "Running run.cpp" << std::endl;
+
+  // mixtures
+
+  std::cout << "End of run.cpp" << std::endl;
+  return 0;
+}
