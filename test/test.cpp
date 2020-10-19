b9a165cafa49929ae3f86ee87140b5d282498d0e
Bruno Guindani <bruno@guindani.org>
2020-10-19 14:54:57 +0200

    Added Mix classes in MWE

--- test/test.cpp
+++ test/test.cpp
@@ -39,18 +39,40 @@ public:
 
 
 
+// Mix classes
+class Mix {
+public:
+  virtual ~Mix() = default;
+  virtual void print_id() const = 0;
+};
+
+class DP: public Mix{
+public:
+  void print_id() const override {std::cout << "DP" << std::endl;}
+};
+
+class PY: public Mix{
+public:
+  void print_id() const override {std::cout << "PY" << std::endl;}
+};
+
+
+
 // Algo classes
 class Algo {
 private:
   std::shared_ptr<Hier> hier;
   std::shared_ptr<Hyp> hyp;
+  std::shared_ptr<Mix> mix;
 public:
   virtual ~Algo() = default;
   virtual void print_id() const = 0;
   void set_Hier(std::shared_ptr<Hier> hier_){hier = hier_;}
   void set_Hyp(std::shared_ptr<Hyp> hyp_){hyp = hyp_;}
+  void set_Mix(std::shared_ptr<Mix> mix_){mix = mix_;}
   void call_Hier_id() const {hier->print_id();}
   void call_Hyp_id() const {hyp->print_id();}
+  void call_Mix_id() const {mix->print_id();}
 };
 
 class N2: public Algo{
@@ -88,6 +110,15 @@ static void load_Hier(){
 }
 
 __attribute__((constructor))
+static void load_Mix(){
+  Factory<Mix> &factory = Factory<Mix>::Instance();
+  Builder<Mix> DPbuilder = [](){return std::make_shared<DP>();};
+  Builder<Mix> PYbuilder = [](){return std::make_shared<PY>();};
+  factory.add_builder("DP", DPbuilder);
+  factory.add_builder("PY", PYbuilder);
+}
+
+__attribute__((constructor))
 static void load_Algo(){
   Factory<Algo> &factory = Factory<Algo>::Instance();
   Builder<Algo> N2builder = [](){return std::make_shared<N2>();};
@@ -99,22 +130,26 @@ static void load_Algo(){
 
 
 // Main
+// Example call: ./test N8 NNW NNWFix PY
 int main(int argc, char const *argv[]){
   std::string type_Algo = argv[1];
   std::string type_Hier = argv[2];
   std::string type_Hyp  = argv[3];
+  std::string type_Mix  = argv[4];
   Factory<Algo> &factory_Algo = Factory<Algo>::Instance();
   Factory<Hier> &factory_Hier = Factory<Hier>::Instance();
-  Factory<Hyp>  &factory_Hyp  = Factory<Hyp >::Instance();
+  Factory<Hyp> &factory_Hyp = Factory<Hyp>::Instance();
+  Factory<Mix> &factory_Mix = Factory<Mix>::Instance();
   auto algo = factory_Algo.create_object(type_Algo);
   auto hier = factory_Hier.create_object(type_Hier);
-  auto hyp  = factory_Hyp.create_object(type_Hyp);
+  auto hyp = factory_Hyp.create_object(type_Hyp);
+  auto mix = factory_Mix.create_object(type_Mix);
   (*algo).set_Hier(hier);
   (*algo).set_Hyp(hyp);
+  (*algo).set_Mix(mix);
   (*algo).print_id();
-  (*hier).print_id();
-  (*hyp ).print_id();
   (*algo).call_Hier_id();
   (*algo).call_Hyp_id();
+  (*algo).call_Mix_id();
   return 0;
 }
