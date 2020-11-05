3fc06404d53b3802e5e1a7c7ec2f8f702db32579
Bruno Guindani <bruno@guindani.org>
2020-11-05 23:35:03 +0100

    Forgot one clone()

--- src/algorithms/MarginalAlgorithm.cpp
+++ src/algorithms/MarginalAlgorithm.cpp
@@ -24,7 +24,7 @@ Eigen::MatrixXd MarginalAlgorithm::eval_lpdf(const Eigen::MatrixXd &grid,
     // Initialize local matrix of log-densities
     Eigen::MatrixXd lpdf_local(grid.rows(), n_clust + 1);
     // Initialize local temporary hierarchy
-    std::shared_ptr<HierarchyBase> temp_hier = unique_values[0];
+    std::shared_ptr<HierarchyBase> temp_hier = unique_values[0]->clone();
 
     // Loop over local unique values i.e. clusters
     for (size_t j = 0; j < n_clust; j++) {
