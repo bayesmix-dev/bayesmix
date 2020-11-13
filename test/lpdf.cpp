e931b07ddbfeafc8054a9c209f6420008d3b9f44
Bruno Guindani <bruno@guindani.org>
2020-11-13 19:15:59 +0100

    IT WERKS

--- test/lpdf.cpp
+++ test/lpdf.cpp
@@ -9,8 +9,6 @@
 #include "../src/hierarchies/nnw_hierarchy.hpp"
 
 TEST(lpdf, nnig) {
-  using namespace stan::math;
-
   NNIGHierarchy hier;
   double mu0 = 5.0;
   double lambda0 = 0.1;
@@ -30,40 +28,26 @@ TEST(lpdf, nnig) {
 
   // Compute posterior parameters
   double mu_n = (lambda0 * mu0 + datum(0)) / (lambda0 + 1);
-  double alpha_n = alpha0;  // + 0.5;
+  double alpha_n = alpha0 + 0.5;
   double lambda_n = lambda0 + 1;
   double beta_n = beta0 + (0.5 * lambda0 / (lambda0 + 1)) * (datum(0) - mu0) *
                               (datum(0) - mu0);
   // equiv.ly: beta0 + 0.5*(mu0^2*lambda0 + datum^2 - mu_n^2*lambda_n);
 
   // Compute pieces
-  double prior1 = stan::math::inv_gamma_lpdf(sd, alpha0, beta0);
+  double prior1 = stan::math::inv_gamma_lpdf(sd*sd, alpha0, beta0);
   double prior2 = stan::math::normal_lpdf(mean, mu0, sd / sqrt(lambda0));
   double prior = prior1 + prior2;
   double like = hier.lpdf(datum)(0);
-  double post1 = stan::math::inv_gamma_lpdf(sd, alpha_n, beta_n);
+  double post1 = stan::math::inv_gamma_lpdf(sd*sd, alpha_n, beta_n);
   double post2 = stan::math::normal_lpdf(mean, mu_n, sd / sqrt(lambda_n));
   double post = post1 + post2;
+
   // Bayes: logmarg(x) = logprior(phi) + loglik(x|phi) - logpost(phi|x)
   double sum = prior + like - post;
   double marg = hier.marg_lpdf(datum)(0);
 
-  double marg_murphy = 0.5 * log(lambda0) + alpha0 * log(beta0) +
-                       lgamma_fun::fun(alpha_n) - 0.5 * log(lambda_n) -
-                       alpha_n * log(beta_n) - lgamma_fun::fun(alpha0) -
-                       log(2.0) + NEG_LOG_SQRT_TWO_PI;
-
-  // std::cout << "prior1=" << prior1 << std::endl;
-  // std::cout << "prior2=" << prior2 << std::endl;
-  // std::cout << "prior =" << prior << std::endl;
-  // std::cout << "like  =" << like << std::endl;
-  // std::cout << "post1 =" << post1 << std::endl;
-  // std::cout << "post2 =" << post2 << std::endl;
-  // std::cout << "post  =" << post << std::endl;
-  std::cout << "sum   =" << sum << std::endl;
-  std::cout << "marg  =" << marg << std::endl;
-  std::cout << "murphy=" << marg_murphy << std::endl;
-  // ASSERT_EQ(marg, marg_murphy);
+  ASSERT_FLOAT_EQ(sum, marg);
 }
 
 
@@ -141,5 +125,5 @@ TEST(lpdf, nnw) {
   std::cout << "sum   =" << sum << std::endl;
   std::cout << "marg  =" << marg << std::endl;
   std::cout << "murphy=" << marg_murphy << std::endl;
-  // ASSERT_EQ(marg, marg_murphy);
+  ASSERT_FLOAT_EQ(marg, marg_murphy);
 }
