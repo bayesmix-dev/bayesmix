e93175b6df5a6bfa2cb4cfba0b0c627cfaa86310
Bruno Guindani <bruno@guindani.org>
2020-11-18 15:02:20 +0100

    More successful tests!

--- test/priors.cpp
+++ test/priors.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
+#include <memory>
 
 //#include <Eigen/Dense>
 // #include <stan/math/prim/fun.hpp>
 // #include <stan/math/prim/prob.hpp>
 
 //#include "../proto/cpp/marginal_state.pb.h"
+#include "../src/hierarchies/base_hierarchy.hpp"
 //#include "../src/hierarchies/nnig_hierarchy.hpp"
 #include "../src/mixings/dirichlet_mixing.hpp"
 
@@ -14,9 +16,36 @@ TEST(mixing, fixed_value) {
   bayesmix::DPState state;
   double m = 2.0;
   state.mutable_fixed_value()->set_value(m);
-  double m_state = state.mutable_fixed_value()->value();
+  double m_state = state.fixed_value().value();
   ASSERT_FLOAT_EQ(m, m_state);
   mix.set_state(&state);
   double m_mix = mix.get_totalmass();
   ASSERT_FLOAT_EQ(m, m_mix);
+
+  std::vector<std::shared_ptr<BaseHierarchy>> hiers(5);
+  mix.update_hypers(hiers, 100);
+  double m_mix_after = mix.get_totalmass();
+  ASSERT_FLOAT_EQ(m, m_mix_after);
+}
+
+
+
+TEST(mixing, gamma_prior) {
+  DirichletMixing mix;
+  bayesmix::DPState state;
+  double alpha = 1.0;
+  double beta = 2.0;
+  double m_prior = alpha / beta;
+  state.mutable_gamma_prior()->set_alpha(alpha);
+  state.mutable_gamma_prior()->set_beta(beta);
+  mix.set_state(&state);
+  double m_mix = mix.get_totalmass();
+  ASSERT_FLOAT_EQ(m_prior, m_mix);
+
+  std::vector<std::shared_ptr<BaseHierarchy>> hiers(5);
+  mix.update_hypers(hiers, 100);
+  double m_mix_after = mix.get_totalmass();
+
+  std::cout << "[          ] after = " << m_mix_after << std::endl;
+  ASSERT_TRUE(m_mix != m_mix_after);
 }
