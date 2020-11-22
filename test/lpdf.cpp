3b4badbabb1e5d9a3b92663d9029eb23fbfa2b7b
Bruno Guindani <bruno@guindani.org>
2020-11-22 18:19:17 +0100

    More correction to tests

--- test/lpdf.cpp
+++ test/lpdf.cpp
@@ -18,7 +18,7 @@ TEST(lpdf, nnig) {
   hier_prior.mutable_fixed_values()->set_mean(mu0);
   hier_prior.mutable_fixed_values()->set_var_scaling(lambda0);
   hier_prior.mutable_fixed_values()->set_shape(alpha0);
-  hier_prior.mutable_fixed_values()->set_rate(beta0);
+  hier_prior.mutable_fixed_values()->set_scale(beta0);
   hier.set_prior(hier_prior);
   hier.initialize();
 
