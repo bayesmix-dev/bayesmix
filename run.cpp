96be170d4d53421abd408a306149866c3cbe09d8
Bruno Guindani <bruno@guindani.org>
2020-11-24 12:16:34 +0100

    Run.cpp reorganization

--- run.cpp
+++ run.cpp
@@ -8,34 +8,33 @@
 int main(int argc, char *argv[]) {
   std::cout << "Running run.cpp" << std::endl;
 
-  // Console parameters (temporarily assigned at compile-time)
+  // Algorithm parameters
+  unsigned int init = 0;
+  unsigned int maxiter = 1000;
+  unsigned int burnin = 1;
+  int rng_seed = 20201103;
+
+  // Get console parameters
   std::string algo_type = argv[1];
   std::string hier_type = argv[2];
   std::string hier_args = argv[3];
   std::string mix_type = argv[4];
   std::string mix_args = argv[5];
-  std::string datafile = "resources/data_uni.csv";  // TEST
-  std::string gridfile = "resources/grid_uni.csv";  // TEST
-  std::string densfile = "resources/dens_uni.csv";  // TEST
-  std::string massfile = "resources/mass_uni.csv";  // TEST
-  std::string nclufile = "resources/nclu_uni.csv";  // TEST
-  unsigned int init = 0;
-  unsigned int maxiter = 1000;
-  unsigned int burnin = 1;
-  int rng_seed = 20201103;
+  std::string datafile = "resources/data_uni.csv";
+  std::string gridfile = "resources/grid_uni.csv";
+  std::string densfile = "resources/dens_uni.csv";
+  std::string massfile = "resources/mass_uni.csv";
+  std::string nclufile = "resources/nclu_uni.csv";
 
   // Create factories and objects
   auto &factory_algo = Factory<BaseAlgorithm>::Instance();
   auto &factory_hier = Factory<BaseHierarchy>::Instance();
   auto &factory_mixing = Factory<BaseMixing>::Instance();
   auto algo = factory_algo.create_object(algo_type);
-  // auto hier = factory_hier.create_object(hier_type);
-  auto hier = std::make_shared<NNIGHierarchy>();  // TEST
+  auto hier = factory_hier.create_object(hier_type);
   auto mixing = factory_mixing.create_object(mix_type);
-
-  // Initialize RNG object
-  auto &rng = bayesmix::Rng::Instance().get();
-  rng.seed(rng_seed);
+  BaseCollector<bayesmix::MarginalState> *coll =
+      new MemoryCollector<bayesmix::MarginalState>();
 
   // Set mixing hyperprior
   std::string mix_prior_str = "bayesmix." + mix_type + "Prior";
@@ -97,24 +96,25 @@ int main(int argc, char *argv[]) {
   // bayesmix::to_proto(tau0,
   // hier_prior.mutable_fixed_values()->mutable_scale());
 
-  // Set parameters
-  algo->set_maxiter(maxiter);
-  algo->set_burnin(burnin);
+  // Initialize RNG object
+  auto &rng = bayesmix::Rng::Instance().get();
+  rng.seed(rng_seed);
 
-  // Read data objects
+  // Read data matrices
   Eigen::MatrixXd data = bayesmix::read_eigen_matrix(datafile);
   Eigen::MatrixXd grid = bayesmix::read_eigen_matrix(gridfile);
 
-  // STUFF:
-  // Object allocation
+  // Set algorithm parameters
+  algo->set_maxiter(maxiter);
+  algo->set_burnin(burnin);
+
+  // Allocate objects in algorithm
   algo->set_mixing(mixing);
   algo->set_data(data);
   algo->set_initial_clusters(hier, init);
   if (algo_type == "N8") {
     algo->set_n_aux(3);
   }
-  BaseCollector<bayesmix::MarginalState> *coll =
-      new MemoryCollector<bayesmix::MarginalState>();
 
   // Run algorithm and density evaluation
   algo->run(coll);
@@ -124,7 +124,7 @@ int main(int argc, char *argv[]) {
   bayesmix::write_matrix_to_file(dens, densfile);
   std::cout << "Successfully wrote density to " << densfile << std::endl;
 
-  // Collect mixing states
+  // Collect mixing and cluster states
   auto chain = coll->get_chain();
   Eigen::VectorXd masses(chain.size());
   Eigen::VectorXd num_clust(chain.size());
@@ -135,6 +135,8 @@ int main(int argc, char *argv[]) {
       num_clust[i] = chain[i].cluster_states_size();
     }
   }
+
+  // Write mixing and cluster states
   bayesmix::write_matrix_to_file(masses, massfile);
   std::cout << "Successfully wrote total masses to " << massfile << std::endl;
   bayesmix::write_matrix_to_file(num_clust, nclufile);
