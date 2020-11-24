a9bcd2b3bf949a78e8f6489853045f152fc2bb3a
Bruno Guindani <bruno@guindani.org>
2020-11-24 11:00:51 +0100

    Correction to compile_protos

--- bash/compile_protos.sh
+++ bash/compile_protos.sh
@@ -3,8 +3,10 @@
 PROTO_DIR="./proto"
 
 for filename in $PROTO_DIR/*.proto; do
-  lib/protobuf/src/protoc --proto_path=$PROTO_DIR \
-  --cpp_out=$PROTO_DIR/cpp/ $filename
+  #lib/protobuf/src/protoc --proto_path=$PROTO_DIR \
+  #--cpp_out=$PROTO_DIR/cpp/ $filename
   lib/protobuf/src/protoc --proto_path=$PROTO_DIR \
   --python_out=$PROTO_DIR/py/ $filename
 done
+
+2to3 --output-dir=$PROTO_DIR/py/ -W -n $PROTO_DIR/py/
