d188f04fec672f0c30321eb5363d578d40f20fc8
mberaha <berahamario@gmail.com>
2020-11-25 10:16:43 +0100

    some new utils

--- src/utils/distributions.hpp
+++ src/utils/distributions.hpp
@@ -1,16 +1,39 @@
 #ifndef BAYESMIX_UTILS_DISTRIBUTIONS_HPP_
 #define BAYESMIX_UTILS_DISTRIBUTIONS_HPP_
 
+#include <omp.h>
+#include <proto/cpp/marginal_state.pb.h>
+
 #include <Eigen/Dense>
 #include <random>
+#include <vector>
 
 namespace bayesmix {
+
 int categorical_rng(const Eigen::VectorXd &probas, std::mt19937_64 &rng,
                     int start = 0);
 double multi_normal_prec_lpdf(const Eigen::VectorXd &datum,
                               const Eigen::VectorXd &mean,
                               const Eigen::MatrixXd &prec_chol,
                               double prec_logdet);
+
+double gaussian_mixture_dist(Eigen::VectorXd means1, Eigen::VectorXd sds1,
+                             Eigen::VectorXd weights1, Eigen::VectorXd means2,
+                             Eigen::VectorXd sds2, Eigen::VectorXd weights2);
+
+double gaussian_mixture_dist(std::vector<Eigen::VectorXd> means1,
+                             std::vector<Eigen::MatrixXd> precs1,
+                             Eigen::VectorXd weights1,
+                             std::vector<Eigen::VectorXd> means2,
+                             std::vector<Eigen::MatrixXd> precs2,
+                             Eigen::VectorXd weights2);
+
+double gaussian_mixture_dist(
+    std::vector<bayesmix::MarginalState::ClusterState> clus1,
+    Eigen::VectorXd weights1,
+    std::vector<bayesmix::MarginalState::ClusterState> clus2,
+    Eigen::VectorXd weights2);
+
 }  // namespace bayesmix
 
 #endif  // BAYESMIX_UTILS_DISTRIBUTIONS_HPP_
---
+++ src/utils/eigen_utils.hpp
@@ -0,0 +1,16 @@
+#include <Eigen/Dense>
+#include <vector>
+
+namespace bayesmix {
+// Stacks a list of matrices by row, i.e. concatenating them one
+// on top of the other
+// TODO test
+Eigen::MatrixXd vstack(const std::vector<Eigen::MatrixXd> &mats);
+
+// TODO test
+void append_by_row(Eigen::MatrixXd *a, const Eigen::MatrixXd &b);
+
+Eigen::MatrixXd append_by_row(const Eigen::MatrixXd &a,
+                              const Eigen::MatrixXd &b);
+
+}  // namespace bayesmix
