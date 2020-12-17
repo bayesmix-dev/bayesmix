7f3ea1a1c48056c4cb36b9de923ebf9e4e1d6851
mberaha <berahamario@gmail.com>
2020-12-17 15:14:51 +0100

    some changes

--- bash/install_libs.sh
+++ bash/install_libs.sh
@@ -5,19 +5,10 @@
 
 # Get libraries
 cd lib
-git clone https://github.com/protocolbuffers/protobuf.git --depth 1
 git clone https://github.com/stan-dev/math.git --depth 1
 
 # Compile protobuf library
-cd protobuf
-./autogen.sh
-./configure
-make
-make check
-sudo make install
-sudo ldconfig
-cd ../..
 
 # Install Python tools
-python3 -m pip install 2to3 matplotlib pybind11 scipy scikit-learn
-python3 -m pip install --user --upgrade google-api-python-client
+#python3 -m pip install 2to3 matplotlib pybind11 scipy scikit-learn
+#python3 -m pip install --user --upgrade google-api-python-client
