dd37317fa85cb3905207f105063713f41e6055d3
mberaha <berahamario@gmail.com>
2020-12-29 09:43:57 +0100

    angle to quote import

--- src/algorithms/base_algorithm.hpp
+++ src/algorithms/base_algorithm.hpp
@@ -5,10 +5,10 @@
 #include <memory>
 #include <vector>
 
-#include <marginal_state.pb.h>
 #include "../collectors/base_collector.hpp"
 #include "../hierarchies/base_hierarchy.hpp"
 #include "../mixings/base_mixing.hpp"
+#include "marginal_state.pb.h"
 
 //! Abstract template class for a Gibbs sampling iterative BNP algorithm.
 
