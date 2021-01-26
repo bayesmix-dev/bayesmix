0ca4e6a4c15cd57d0815bba3fe8bea957d4d08fd
mberaha <berahamario@gmail.com>
2021-01-26 08:11:06 +0100

    now finally finding local protobuf

--- FindProtobuf.cmake
+++ FindProtobuf.cmake
@@ -23,15 +23,16 @@ Protobuf_FOUND          - True if Protobuf found.
 find_package(PkgConfig REQUIRED)
 
 pkg_check_modules(PROTOBUF protobuf>=3.12 IMPORTED_TARGET GLOBAL)
-if (Protobuf_FOUND)
-add_library(protobuf::libprotobuf ALIAS PkgConfig::PROTOBUF)
-set_target_properties(PkgConfig::PROTOBUF PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${PROTOBUF_INCLUDEDIR}")
 
-find_program(PROTOC_EXEC protoc)
-add_executable(protobuf::protoc IMPORTED GLOBAL)
-set_target_properties(protobuf::protoc PROPERTIES
-  IMPORTED_LOCATION ${PROTOC_EXEC}
-)
+if (PROTOBUF_FOUND)
+  add_library(protobuf::libprotobuf ALIAS PkgConfig::PROTOBUF)
+  set_target_properties(PkgConfig::PROTOBUF PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${PROTOBUF_INCLUDEDIR}")
+
+  find_program(PROTOC_EXEC protoc)
+  add_executable(protobuf::protoc IMPORTED GLOBAL)
+  set_target_properties(protobuf::protoc PROPERTIES
+    IMPORTED_LOCATION ${PROTOC_EXEC}
+  )
 endif()
 
