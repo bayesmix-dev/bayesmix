8b3fbe3f227da2340a5667ff18514c0e163a6803
mberaha <berahamario@gmail.com>
2021-02-01 17:00:42 +0100

    moved conjugacy checks in basealgo

--- src/algorithms/neal2_algorithm.cc
+++ src/algorithms/neal2_algorithm.cc
@@ -89,18 +89,14 @@ Eigen::VectorXd Neal2Algorithm::get_cluster_lpdf(
 
 void Neal2Algorithm::print_startup_message() const {
   std::string msg = "Running Neal2 algorithm with " +
-                    bayesmix::Hierarchy_Name(unique_values[0]->get_id()) +
+                    bayesmix::HierarchyId_Name(unique_values[0]->get_id()) +
                     " hierarchies, " +
-                    bayesmix::Mixing_Name(mixing->get_id()) + " mixing...";
+                    bayesmix::MixingId_Name(mixing->get_id()) + " mixing...";
   std::cout << msg << std::endl;
 }
 
 void Neal2Algorithm::initialize() {
   BaseAlgorithm::initialize();
-  if (unique_values[0]->is_conjugate() == false) {
-    throw std::invalid_argument(
-        "This algorithm only supports conjugate hierarchies");
-  }
 }
 
 void Neal2Algorithm::sample_allocations() {
