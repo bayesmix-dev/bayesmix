94a5d7e8753b570cb7c143c56c0f1bda0b0a4dee
brunoguindani <bruno@guindani.org>
2021-02-04 16:11:17 +0100

    Neal3 joins the fray!

---
+++ src/algorithms/neal3_algorithm.cc
@@ -0,0 +1,30 @@
+#include "neal3_algorithm.h"
+
+#include <Eigen/Dense>
+#include <memory>
+
+#include "src/hierarchies/base_hierarchy.h"
+
+Eigen::VectorXd Neal3Algorithm::get_cluster_lpdf(
+    const unsigned int data_idx) const {
+  unsigned int n_data = data.rows();
+  unsigned int n_clust = unique_values.size();
+  Eigen::VectorXd loglpdf(n_clust + 1);
+  for (size_t j = 0; j < n_clust; j++) {
+    // Probability of being assigned to an already existing cluster
+    loglpdf(j) = unique_values[j]->marg_lpdf(true, data.row(data_idx),
+                                             hier_covariates.row(data_idx));
+  }
+  // Probability of being assigned to a newly created cluster
+  loglpdf(n_clust) = unique_values[0]->marg_lpdf(
+      false, data.row(data_idx), hier_covariates.row(data_idx));
+  return loglpdf;
+}
+
+void Neal3Algorithm::print_startup_message() const {
+  std::string msg = "Running Neal3 algorithm with " +
+                    bayesmix::HierarchyId_Name(unique_values[0]->get_id()) +
+                    " hierarchies, " +
+                    bayesmix::MixingId_Name(mixing->get_id()) + " mixing...";
+  std::cout << msg << std::endl;
+}
