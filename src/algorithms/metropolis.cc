fa564c4e1de923bf2140626ee184f567a125a4e8
brunoguindani <bruno@guindani.org>
2021-02-15 23:16:44 +0100

    Going further with Metropolis

--- src/algorithms/metropolis.cc
+++ src/algorithms/metropolis.cc
@@ -2,15 +2,17 @@
 
 #include <stan/math/prim.hpp>
 
-// MALA: invece che in alpha, la proposal viene centrata in
-// alpha + tau * grad(log(target(alpha)))
-
 double Metropolis::standard_mean() const {
   return state;
 }
 
 double Metropolis::mala_mean() const {
-  return state;  // TODO
+  Eigen::VectorXd grad = (-1.0 / true_var) * alpha;
+  for (int i = 0; i < data.rows(); i++) {
+    grad +=
+      (data(i) - inv_logit(state.dot(covariates.row(i)))) * covariates.row(i);
+  }
+  return state + tau * grad;
 }
 
 void Metropolis::metropolis_hastings_step() {
@@ -18,8 +20,18 @@ void Metropolis::metropolis_hastings_step() {
   Eigen::VectorXd mean = use_mala : mala_mean() ? standard_mean();
   auto covar = var * Eigen::MatrixXd::Identity(dim, dim);
   Eigen::VectorXd proposed = stan::math::multi_normal_rng(mean, covar, rng);
-  // Compute acceptance ratio
-  double ratio = 1.0;  // TODO remember min!
+  double ratio1 = std::exp((-0.5/true_var)*(proposed.dot(proposed) -
+    state.dot(state)));
+  double ratio2 = 1.0;
+  for (int i = 0; i < data.rows(); i++) {
+    ratio2 *= std::exp(data.row(i) * covariates.row(i).dot(proposed)) /
+      (1+std::exp(covariates.row(i).dot(proposed)));
+  }
+  double ratio3 = std::exp(
+    (-0.5 / prop_var) *
+      ( (mean-proposed).dot(mean-proposed) - (mean-alpha).dot(mean-alpha) )
+    );
+  double ratio = ratio1 * ratio2 * ratio3;
   // Accept with probability ratio
   double p = stan::math::uniform_rng(0.0, 1.0, rng);
   if (p <= ratio) {
--- src/algorithms/metropolis.h
+++ src/algorithms/metropolis.h
@@ -5,19 +5,37 @@
 
 #include "src/utils/rng.h"
 
+// Model: y_i | alpha ~ Bern( logit^-1(x'_i alpha) )
+//              alpha ~ N(0, Lambda^-1)
+//          Lambda^-1 = sig2 * I
+// MALA proposal density: h(x) = N(a_M, eta * I) with
+//                         a_M = alpha + tau * grad(log(f(alpha|rest)))
+// Here grad(...) = -Lambda alpha + sum_i (y_i logit^-1(x'_i alpha)) x_i
+
 class Metropolis {
  protected:
   unsigned int iter = 0;
   unsigned int maxiter = 5000;
   auto &rng = bayesmix::Rng::Instance().get();
-  //! Design parameters
-  double var = 1.0;
-  double tau = 1.0;
+
+  // DESIGN PARAMETERS
+  //! Penalization parameter aka tau
+  double penal = 0.1;
+  //! Proposed variance aka eta
+  double prop_var = 2.0;
+  //!
   bool use_mala;
-  //! State
+
+  // DATA
+  Eigen::VectorXd data;
+  Eigen::MatrixXd covariates;
   unsigned int dim;
+  //! True variance aka sig2
+  double true_var = 1.0;
+  //! State aka alpha
   Eigen::VectorXd state;
 
+  // UTILITIES
   Eigen::VectorXd standard_mean() const;
   Eigen::VectorXd mala_mean() const;
   void metropolis_hastings_step();
@@ -27,15 +45,23 @@ class Metropolis {
   Metropolis() = default;
   ~Metropolis() = default;
 
+  double inv_logit(const double x) const {
+    return std::exp(x) / (1 + std::exp(x));
+  }
+
   void set_dim(const unsigned int dim_) {
     dim = dim_;
     state = Eigen::VectorXd::Zero(dim);
   }
-
-  void set_var(const double var_) { var = var_; }
+  void set_prop_var(const double var_) { prop_var = var_; }
+  void set_true_var(const double var_) { true_var = var_; }
+  void set_penal(const double penal_) { penal = penal_; }
+  void set_data(const Eigen::VectorXd &data_) { data = data_; }
+  void set_covariates(const Eigen::MatrixXd &covar_) { covariates = covar_; }
 
   void run(const bool use_mala_) {
     use_mala = use_mala_;
+    state = Eigen::VectorXd::Zero(dim);  // TODO?
     while(iter < maxiter) {
       metropolis_hastings_step();
       output();
