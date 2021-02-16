2b8775b07b0241e3143764b3d34acdbd0c9b9d30
João Oliveira <51805601+JoaoHenriqueOliveira@users.noreply.github.com>
2021-02-16 20:39:23 +0100

    Updated header file (commented)

--- src/clustering/uncertainty/CredibleBall.hpp
+++ src/clustering/uncertainty/CredibleBall.hpp
@@ -4,7 +4,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <set>
-#include <vector>
 
 #include <Eigen/Dense>
 #include "../lossfunction/LossFunction.hpp"
@@ -18,23 +17,30 @@ class CredibleBall {
   Eigen::MatrixXi mcmc_sample;     // MCMC matrix of clusters
   Eigen::VectorXi point_estimate;  // output of the greedy algorithm
   set<int, greater<int>>
-      credibleBall;  // set of index clusters inside the credible region
-  int T;             // number of clusters, ie mcmc_sample.rows
-  int N;             // dimension of cluster, ie mcmc_sample.cols
-  double alpha;      // level of the credible ball region
-  double radius;     // radius of the credible ball
+      credibleBall;     // set of index clusters inside the credible region
+  int T;                // number of clusters, ie mcmc_sample.rows
+  int N;                // dimension of cluster, ie mcmc_sample.cols
+  double alpha;         // level of the credible ball region
+  double radius;        // radius of the credible ball
+  double prob;          // estimated probability
+  double vlb_distance;  // distance to the vertical lower bounds
+  double vub_distance;  // distance to the vertical upper bounds
+  double hb_distance;   // distance to the horizontal bounds
 
  public:
   CredibleBall(LOSS_FUNCTION loss_type_, Eigen::MatrixXi& mcmc_sample_,
                double alpha_, Eigen::VectorXi& point_estimate_);
   ~CredibleBall();
+
   double calculateRegion(double rate);   // calculate the radius
   Eigen::VectorXi VerticalUpperBound();  // index of cluusters of the VUB
   Eigen::VectorXi VerticalLowerBound();  // index of the clusters of the VLB
   Eigen::VectorXi HorizontalBound();     // index of the clusters of the HB
 
- private:
   void populateCredibleSet();  // populate the credibleBall
   int count_cluster_row(int index);
+  void sumary(
+      Eigen::VectorXi HB, Eigen::VectorXi VUB, Eigen::VectorXi VLB,
+      string filename);  // generates a file with the bounds information
 };
 #endif  // CREDIBLE_BALL_HPP
