a2e0ecc6af61caef4a8619de2f8b68bcbcc3af72
João Oliveira <51805601+JoaoHenriqueOliveira@users.noreply.github.com>
2021-02-16 20:49:22 +0100

    final run_cb (with summary at the end)

--- run_cb.cpp
+++ run_cb.cpp
@@ -12,6 +12,7 @@ using namespace Eigen;
 
 template <typename M>
 M load_csv1(const std::string &path) {
+  // Use this if csv is separated with ","
   std::ifstream indata;
   indata.open(path);
   std::string line;
@@ -29,8 +30,10 @@ M load_csv1(const std::string &path) {
                           M::ColsAtCompileTime, RowMajor>>(
       values.data(), rows, values.size() / rows);
 }
+
 template <typename M>
 M load_csv2(const std::string &path) {
+  // use this if csv is searated with spaces " "
   std::ifstream indata;
   indata.open(path);
   std::string line;
@@ -61,48 +64,50 @@ int main(int argc, char const *argv[]) {
   string filename_pe = argv[2];
   string filename_out = argv[3];
   int loss_type = std::stoi(argv[4]);
-  double learning_rate = stoi(argv[5]);
+  double learning_rate =
+      stoi(argv[5]);  // a good choice for "learning_rate" is 1 to 5
   cout << "Learning rate: " << learning_rate << endl;
 
   Eigen::MatrixXi mcmc, pe_tmp;
   Eigen::VectorXi pe;
-  cout << "ok1" << endl;
-  pe_tmp = load_csv1<MatrixXi>(filename_pe);
-  cout << "ok2" << endl;
-  pe = pe_tmp.row(0);
-  cout << pe_tmp.cols() << endl;
-  cout << "ok3" << endl;
+
   mcmc = load_csv2<MatrixXi>(filename_mcmc);
+  pe_tmp = load_csv1<MatrixXi>(filename_pe);
+
+  pe = pe_tmp.row(0);
+  cout << "Dimension of the point estimate: " << pe_tmp.cols() << endl;
 
   cout << "Matrix with dimensions : " << mcmc.rows() << "*" << mcmc.cols()
        << " found." << endl;
 
   CredibleBall CB =
       CredibleBall(static_cast<LOSS_FUNCTION>(loss_type), mcmc, 0.05, pe);
-  cout << "ok4" << endl;
   double r = CB.calculateRegion(learning_rate);
   cout << "radius: " << r << "\n";
 
+  cout << "Vertical Upper Bound\n";
   Eigen::VectorXi VUB = CB.VerticalUpperBound();
   cout << "VUB cardinality: " << VUB.size() << " \n";
   for (int i = 0; i < VUB.size(); i++) {
     cout << "Index of the VUB element:" << VUB(i) << " ";
   }
   cout << "\n";
-
+  cout << "Vertical Lower Bound\n";
   Eigen::VectorXi VLB = CB.VerticalLowerBound();
   cout << "VLB cardinality: " << VLB.size() << " \n";
   for (int i = 0; i < VLB.size(); i++) {
     cout << "Index of the VLB element:" << VLB(i) << " ";
   }
   cout << "\n";
-
+  cout << "Horizontal Bound\n";
   Eigen::VectorXi HB = CB.HorizontalBound();
   cout << "HB cardinality: " << HB.size() << " \n";
   for (int i = 0; i < HB.size(); i++) {
     cout << "Index of the HB element:" << HB(i) << " ";
   }
   cout << "\n";
+
+  CB.sumary(HB, VUB, VLB, filename_out);
 
   return 0;
 }
