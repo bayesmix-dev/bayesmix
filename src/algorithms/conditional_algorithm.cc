4f0833605944e37f1ad389c532fe9e81968bd3cb
brunoguindani <bruno@guindani.org>
2021-02-18 22:02:58 +0100

    First changes to ConditionalAlgorithm

--- src/algorithms/conditional_algorithm.cc
+++ src/algorithms/conditional_algorithm.cc
@@ -2,7 +2,13 @@
 
 #include <Eigen/Dense>
 
+#include "base_algorithm.h"
 #include "src/collectors/base_collector.h"
+
+void ConditionalAlgorithm::initialize() {
+  BaseAlgorithm::initialize();
+  weights = VectorXd::Ones(init_num_clusters) / init_num_clusters;
+}
 
 //! \param grid Grid of points in matrix form to evaluate the density on
 //! \param coll Collector containing the algorithm chain
@@ -10,5 +16,5 @@ Eigen::MatrixXd ConditionalAlgorithm::eval_lpdf(
     BaseCollector *const collector, const Eigen::MatrixXd &grid,
     const Eigen::MatrixXd &hier_covariates /*= Eigen::MatrixXd(0, 0)*/,
     const Eigen::MatrixXd &mix_covariates /*= Eigen::MatrixXd(0, 0)*/) const {
-  return Eigen::MatrixXd::Zero(1, 1);
+  return Eigen::MatrixXd::Zero(1, 1);  // TODO
 }
--- src/algorithms/conditional_algorithm.h
+++ src/algorithms/conditional_algorithm.h
@@ -5,11 +5,23 @@
 
 #include "base_algorithm.h"
 #include "src/collectors/base_collector.h"
+#include "src/mixings/base_mixing.h"
 
 class ConditionalAlgorithm : public BaseAlgorithm {
+ protected:
+  Eigen::VectorXd weights;
+
  public:
   ~ConditionalAlgorithm() = default;
   ConditionalAlgorithm() = default;
+  //!
+  void initialize() override;
+  //!
+  void step() override {
+    BaseAlgorithm::step();
+    mixing->update_state(unique_values, data.size());
+  }
+  //!
   virtual Eigen::MatrixXd eval_lpdf(
       BaseCollector *const collector, const Eigen::MatrixXd &grid,
       const Eigen::MatrixXd &hier_covariates = Eigen::MatrixXd(0, 0),
