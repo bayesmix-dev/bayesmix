f5348b308bc5d1d9aa388b495e54bb0900ef97ed
brunoguindani <bruno@guindani.org>
2021-03-22 15:18:15 +0100

    NNW lpdf test WIP

--- test/lpdf.cc
+++ test/lpdf.cc
@@ -54,88 +54,6 @@ TEST(lpdf, nnig) {
   ASSERT_DOUBLE_EQ(sum, marg);
 }
 
-// TEST(lpdf, nnw) {  // TODO
-//   using namespace stan::math;
-//   NNWHierarchy hier;
-//   bayesmix::NNWPrior hier_prior;
-//   Eigen::Vector2d mu0; mu0 << 5.5, 5.5;
-//   bayesmix::Vector mu0_proto;
-//   bayesmix::to_proto(mu0, &mu0_proto);
-//   double lambda0 = 0.2;
-//   double nu0 = 5.0;
-//   Eigen::Matrix2d tau0 = Eigen::Matrix2d::Identity() / nu0;
-//   bayesmix::Matrix tau0_proto;
-//   bayesmix::to_proto(tau0, &tau0_proto);
-//   *hier_prior.mutable_fixed_values()->mutable_mean() = mu0_proto;
-//   hier_prior.mutable_fixed_values()->set_var_scaling(lambda0);
-//   hier_prior.mutable_fixed_values()->set_deg_free(nu0);
-//   *hier_prior.mutable_fixed_values()->mutable_scale() = tau0_proto;
-//   hier.set_prior(hier_prior);
-//   hier.initialize();
-//
-//   Eigen::VectorXd mu = mu0;
-//   Eigen::MatrixXd tau = lambda0 * Eigen::Matrix2d::Identity();
-//
-//   Eigen::RowVectorXd datum(2);
-//   datum << 4.5, 4.5;
-//
-//   // Compute prior parameters
-//   Eigen::MatrixXd tau_pr = lambda0 * tau0;
-//
-//   // Compute posterior parameters
-//   double mu_n = (lambda0 * mu0 + datum(0)) / (lambda0 + 1);
-//   double alpha_n = alpha0 + 0.5;
-//   double lambda_n = lambda0 + 1;
-//   double nu_n = nu0 + 0.5;
-//   Eigen::VectorXd mu_n =
-//       (lambda0 * mu0 + datum.transpose()) / (lambda0 + 1);
-//   Eigen::MatrixXd tau_temp =
-//       stan::math::inverse_spd(tau0) + (0.5 * lambda0 / (lambda0 + 1)) *
-//                                           (datum.transpose() - mu0) *
-//                                           (datum - mu0.transpose());
-//   Eigen::MatrixXd tau_n = stan::math::inverse_spd(tau_temp);
-//   Eigen::MatrixXd tau_post = lambda_n * tau_n;
-//
-//   // Compute pieces
-//   double prior1 = stan::math::wishart_lpdf(tau, nu0, tau0);
-//   double prior2 = stan::math::multi_normal_prec_lpdf(mu, mu0, tau_pr);
-//   double prior = prior1 + prior2;
-//   double like = hier.like_lpdf(datum);
-//   double post1 = stan::math::wishart_lpdf(tau, nu_n, tau_post);
-//   double post2 = stan::math::multi_normal_prec_lpdf(mu, mu0, tau_post);
-//   double post = post1 + post2;
-//
-//   // Bayes: logmarg(x) = logprior(phi) + loglik(x|phi) - logpost(phi|x)
-//   double sum = prior + like - post;
-//   double marg = hier.prior_pred_lpdf(false, datum);
-//
-//   // Compute logdet's
-//   Eigen::MatrixXd tauchol0 =
-//       Eigen::LLT<Eigen::MatrixXd>(tau0).matrixL().transpose();
-//   double logdet0 = 2 * log(tauchol0.diagonal().array()).sum();
-//   Eigen::MatrixXd tauchol_n =
-//       Eigen::LLT<Eigen::MatrixXd>(tau_n).matrixL().transpose();
-//   double logdet_n = 2 * log(tauchol_n.diagonal().array()).sum();
-//
-//   // lmgamma(dim, x)
-//   int dim = 2;
-//   double marg_murphy = lmgamma(dim, 0.5 * nu_n) + 0.5 * nu_n * logdet_n +
-//                        0.5 * dim * log(lambda0) + dim * NEG_LOG_SQRT_TWO_PI
-//                        - lmgamma(dim, 0.5 * nu0) - 0.5 * nu0 * logdet0 - 0.5
-//                        * dim * log(lambda_n);
-//
-//   // std::cout << "prior1=" << prior1 << std::endl;
-//   // std::cout << "prior2=" << prior2 << std::endl;
-//   // std::cout << "prior =" << prior << std::endl;
-//   // std::cout << "like  =" << like << std::endl;
-//   // std::cout << "post1 =" << post1 << std::endl;
-//   // std::cout << "post2 =" << post2 << std::endl;
-//   // std::cout << "post  =" << post << std::endl;
-//   std::cout << "sum   =" << sum << std::endl;
-//   std::cout << "marg  =" << marg << std::endl;
-//   std::cout << "murphy=" << marg_murphy << std::endl;
-//   ASSERT_DOUBLE_EQ(marg, marg_murphy);
-// }
 
 TEST(lpdf, lin_reg_uni) {
   // Create hierarchy objects
@@ -196,4 +114,63 @@ TEST(lpdf, lin_reg_uni) {
   double marg = hier.prior_pred_lpdf(datum, cov);
 
   ASSERT_FLOAT_EQ(sum, marg);
+}
+
+TEST(lpdf, nnw) {
+  using namespace stan::math;
+  // Initialize prior and hierarchy
+  NNWHierarchy hier;
+  bayesmix::NNWPrior prior;
+  Eigen::VectorXd mu0(2);
+  mu0 << 5.5, 5.5;
+  bayesmix::Vector mu0_proto;
+  bayesmix::to_proto(mu0, &mu0_proto);
+  double lambda0 = 0.2;
+  double nu0 = 5.0;
+  Eigen::Matrix2d tau0 = Eigen::Matrix2d::Identity();
+  bayesmix::Matrix tau0_proto;
+  bayesmix::to_proto(tau0, &tau0_proto);
+  *prior.mutable_fixed_values()->mutable_mean() = mu0_proto;
+  prior.mutable_fixed_values()->set_var_scaling(lambda0);
+  prior.mutable_fixed_values()->set_deg_free(nu0);
+  *prior.mutable_fixed_values()->mutable_scale() = tau0_proto;
+  hier.get_mutable_prior()->CopyFrom(prior);
+  hier.initialize();
+  // std::cout << hier.get_posterior_hypers().mean << std::endl;
+  // Reproduce hierarchy state
+  Eigen::VectorXd mu = mu0;
+  Eigen::MatrixXd tau = lambda0 * Eigen::Matrix2d::Identity();
+  // Datum
+  Eigen::RowVectorXd datum(2);
+  datum << 4.5, 4.5;
+  hier.add_datum(1, datum, true);
+  // std::cout << hier.get_posterior_hypers().mean << std::endl;
+  // Compute prior
+  double logprior1 = stan::math::wishart_lpdf(tau, nu0, tau0);
+  double logprior2 = stan::math::multi_normal_prec_lpdf(mu, mu0,
+  	                                                    lambda0 * tau0);
+  double logprior = logprior1 + logprior2;
+  // Compute likelihood
+  double loglike = hier.like_lpdf(datum);
+  // Compute posterior
+  auto post = hier.get_posterior_hypers();
+  double logpost1 = stan::math::wishart_lpdf(tau, post.deg_free,
+  	  post.scale_inv);
+  double logpost2 = stan::math::multi_normal_prec_lpdf(mu, post.mean,
+      post.var_scaling * post.scale_inv);
+  double logpost = logpost1 + logpost2;
+  // Bayes: logmarg(x) = logprior(phi) + loglik(x|phi) - logpost(phi|x)
+  double logsum = logprior + loglike - logpost;
+  double logmarg = hier.prior_pred_lpdf(datum);
+  // Print values
+  std::cout << "prior1=" << logprior1 << std::endl;
+  std::cout << "prior2=" << logprior2 << std::endl;
+  std::cout << "prior =" << logprior << std::endl;
+  std::cout << "like  =" << loglike << std::endl;
+  std::cout << "post1 =" << logpost1 << std::endl;
+  std::cout << "post2 =" << logpost2 << std::endl;
+  std::cout << "post  =" << logpost << std::endl;
+  std::cout << "sum   =" << logsum << std::endl;
+  std::cout << "marg  =" << logmarg << std::endl;
+  ASSERT_DOUBLE_EQ(logsum, logmarg);
 }
