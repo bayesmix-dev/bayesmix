d61bce248f47c6b3a7b5a747d6062cf178eeef02
Mario beraha <berahamario@gmail.com>
2021-11-26 15:51:05 +0100

    added docs

--- examples/tutorial/run.sh
+++ examples/tutorial/run.sh
@@ -19,9 +19,9 @@ build/run \
   resources/tutorial/algo.asciipb \
   NNIG resources/tutorial/nnig_ngg.asciipb \
   DP   resources/tutorial/dp_gamma.asciipb \
-  "" \
+  resources/tutorial/out/chains.recordio \
   resources/tutorial/data.csv \
   resources/tutorial/grid.csv \
-  resources/tutorial/out/density.csv \
+  resources/tutorial/out/density_file.csv \
   resources/tutorial/out/numclust.csv \
   resources/tutorial/out/clustering.csv
