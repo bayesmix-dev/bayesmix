4fd8323458fc1d6c0562f48afe29df31df66520b
Mario beraha <berahamario@gmail.com>
2021-11-27 11:18:09 +0100

    added proper readme and some docs

--- python/README.md
+++ python/README.md
@@ -1,1 +1,116 @@
 # BayesMixPy: a Python interface to BayesMix
+
+## Installation
+
+After you have cloned the bayesmix github directory, navigate to the Python subfolder and
+install bayesmixpy using pip
+
+```
+cd python
+pip3 install -e .
+```
+
+## Usage
+
+`bayesmixpy` provides two functions: `build_bayesmix` and `run_mcmc`. The first one
+installs `bayesmix` and its executables for you, while the second one calls the
+executable that runs the MCMC sampler from Python.
+
+### Building bayesmix
+
+To build `bayesmix`, in a Python shell or a notebook write
+
+```
+from bayesmixpy import build_bayesmix
+
+n_proc = 4 # number of processors for building in parallel
+build_bayesmix(n_proc)
+```
+
+this will print out the installation log and, if installation was successful, the following message
+
+```
+Bayesmix executable is in '<BAYESMIX_HOME_REPO>/build',
+export the environment variable BAYESMIX_EXE=<BAYESMIX_HOME_REPO>build/run
+```
+
+Hence, for running the MCMC chain you should export the `BAYESMIX_EXE` environment variable. This can be done once and for all by copying
+
+```
+BAYESMIX_EXE=<BAYESMIX_HOME_REPO>build/run
+```
+
+in your .bashrc file (or .zshrc if you are a MacOs user), or everytime you use bayesmixpy you can add the following lines on top of your Python script/notebook
+
+```
+import os
+os.environ["BAYESMIX_EXE"] = <BAYESMIX_HOME_REPO>build/run
+
+from bayesmixpy import run_mcmc
+....
+```
+
+### Running bayesmix
+
+To `run_mcmc` must define the model and the algorithm in some configuration files or
+text strings. See the documentation for more details.
+
+For instance, to fit a Dirichlet Process Mixture on univariate data using a Normal-Normal-InverseGamma hierarchy using Neal's Algorithm 3, we use the following
+
+```
+eval_dens, n_clus_chain, best_clus = run_mcmc(
+    "NNIG",
+    "DP",
+    data,
+    nnig_params,
+    dp_params,
+    algo_params,
+    dens_grid)
+```
+
+where `data` is a np.array of data points, `dens_grid` is a np.array of points where to evaluate the density, and `nnig_params`, `dp_params` and `algo_params` are defined as follows.
+
+```
+nnig_params="""
+ngg_prior {
+  mean_prior {
+    mean: 5.5
+    var: 2.25
+  }
+  var_scaling_prior {
+    shape: 0.2
+    rate: 0.6
+  }
+  shape: 1.5
+  scale_prior {
+    shape: 4.0
+    rate: 2.0
+  }
+}
+"""
+```
+
+This specifies that the base (centering) measure is a Normal-InverseGamma with parameters (mu0, lam0, a0, b0). Further, mu0 ~ N(5.5, 2.25) lam0 ~ Gamma(0.2, 0.6) a0 = 1.5, b0 ~ Gamma(4.0, 2.0) [See the messages NNIGPrior and NNIGPrior::NGGPrior in the file hierarchy_prior.proto for further reference].
+
+```
+dp_params = """
+gamma_prior {
+  totalmass_prior {
+    shape: 4.0
+    rate: 2.0
+  }
+}
+"""
+```
+
+This specifies that the concentration parameter of the DP has an hyperprior which is a Gamma distribution with parameters (4, 2).
+
+```
+algo_params = """
+algo_id: "Neal3"
+rng_seed: 20201124
+iterations: 2000
+burnin: 1000
+init_num_clusters: 3
+"""
+```
--- python/bayesmixpy/run.py
+++ python/bayesmixpy/run.py
@@ -9,10 +9,6 @@ from pathlib import Path
 from .shell_utils import run_shell
 
 
-BAYESMIX_EXE = os.environ.get("BAYESMIX_EXE", default="")
-RUN_CMD = BAYESMIX_EXE + " {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}"
-
-
 def _is_file(a: str):
     p = Path(a)
     return p.exists() and p.is_file()
@@ -36,7 +32,7 @@ def _maybe_print_to_file(maybe_proto: str,
 
 
 
-def get_filenames(outdir):
+def _get_filenames(outdir):
     data = os.path.join(outdir, 'data.csv')
     dens_grid = os.path.join(outdir, 'dens_grid.csv')
     n_clus = os.path.join(outdir, 'n_clus.csv')
@@ -48,11 +44,46 @@ def run_mcmc(
         hierarchy: str,
         mixing: str,
         data: np.array,
-        dens_grid: np.array,
         hier_params: str,
         mix_params: str,
         algo_params: str,
+        dens_grid: np.array = None,
         out_dir: str = None):
+
+    """
+    Run the MCMC sampling by calling the Bayesmix executable from a subprocess.
+    Arguments
+    ---------
+    hierarchy: the id of the hyerarchy. Must be one of ["NNIG", "NNW", "LinRegUni"]
+    mixing: the id of the mixing. Must be one of ["DP", "PY", "LogSB", "TruncSB"]
+    data: a numpy array of shape (n_samples, dim)
+    hier_params: a text string containing the hyperparameters of the hierarchy or
+        a file name where the hyperparameters are stored. A protobuf message of the
+        corresponding type will be created and populated with the parameters.
+        See the file hierarchy_prior.proto for the corresponding message.
+    mix_params: a text string containing the hyperparameters of the mixing or
+        a file name where the hyperparameters are stored. A protobuf message of the
+        corresponding type will be created and populated with the parameters.
+        See the file mixing_prior.proto for the corresponding message.
+    algo_params: a text string containing the hyperparameters of the algorithm or
+        a file name where the hyperparameters are stored.
+        See the file algorithm_params.proto for the corresponding message.
+    dens_grid: a numpy array of shape (n_dens_grid_points,): points where to evaluate
+        the density. If None, the density will not be evaluated.
+    out_dir: if not None, where to store the output. If None, a temporary directory
+        will be created and destroyed after the sampling is finished.
+
+    Returns
+    -------
+    eval_dens: a numpy array of shape (n_samples, n_dens_grid_points):
+        for each iteration, the mixture density evaluated at the points in dens_grid.
+    n_clus: a numpy array of shape (n_samples,): the number of clusters for each iteration.
+    clus: the best clustering obtained by minimizing Binder's loss function.
+    """
+
+    BAYESMIX_EXE = os.environ.get("BAYESMIX_EXE", default="")
+    RUN_CMD = BAYESMIX_EXE + " {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}"
+
 
     if out_dir is None:
         out_dir = TemporaryDirectory().name
@@ -61,7 +92,7 @@ def run_mcmc(
     else:
         remove_out_dir = False
 
-    data_file, dens_grid_file, nclus_file, clus_file = get_filenames(out_dir)
+    data_file, dens_grid_file, nclus_file, clus_file = _get_filenames(out_dir)
     np.savetxt(data_file, data, delimiter=',')
     np.savetxt(dens_grid_file, dens_grid, delimiter=',')
 
