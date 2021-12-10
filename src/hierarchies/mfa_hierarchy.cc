b8840f3da10cd62712b5645e05ef7dc6b5fb7fb0
giacomodecarlo <giacomo.decarlo@mail.polimi.it>
2021-12-10 17:04:11 +0100

    added .cc and .h files

---
+++ src/hierarchies/mfa_hierarchy.cc
@@ -0,0 +1,248 @@
+#include "mfa_hierarchy.h"
+
+#include <google/protobuf/stubs/casts.h>
+
+#include <Eigen/Dense>
+#include <stan/math/prim/prob.hpp>
+#include <vector>
+
+#include "algorithm_state.pb.h"
+#include "hierarchy_prior.pb.h"
+#include "ls_state.pb.h"
+#include "src/utils/rng.h"
+
+double MFAHierarchy::like_lpdf(const Eigen::RowVectorXd & datum) const {
+
+    return stan::math::normal_lpdf(
+        datum, state.mu + state.Lambda*state.lambda.transpose(), state.psi.cwiseSqrt());
+}
+
+MFA::State MFAHierarchy::draw(const MFA::Hyperparams& params) {
+    auto& rng = bayesmix::Rng::Instance().get();
+    MFA::State out;
+
+    for (size_t j = 0; j < p; j++) {
+        out.mu[j] = stan::math::normal_prec_rng(params.mutilde[j], 1 / sqrt(params.phi), rng);
+        out.psi[j] = stan::math::inv_gamma_rng(params.alpha0, params.beta[j], rng);
+        for (size_t i = 0; i < q; j++) {
+            out.Lambda[j, i] = stan::math::normal_prec_rng(0, 1, rng);
+        }
+    }
+
+    for (size_t i = 0; i < card; i++) {
+        for (size_t j = 0; j < q; j++) {
+            out.Eta[j, i] = stan::math::normal_prec_rng(0, 1, rng);
+        }
+    }
+
+    return out;
+}
+
+void MFAHierarchy::initialize_state() {
+    state.mu = hypers->mutilde;
+    state.psi = hypers->beta / (hypers->alpha0 + 1.);
+    state.Eta = Eigen::MatrixXd::Zero(card,q);
+    state.Lambda = Eigen::MatrixXd::Zero(p,q);
+}
+
+void MFAHierarchy::initialize_hypers() {
+
+    if (prior->has_fixed_values()) {
+        // Set values
+        hypers->mutilde = bayesmix::to_eigen(prior->fixed_values().mutilde());
+        p = hypers->mutilde.size();
+        hypers->beta = bayesmix::to_eigen(prior->fixed_values().beta());
+        hypers->phi = prior->fixed_values().phi();
+        hypers->alpha0 = prior->fixed_values().alpha0();
+        hypers->q = prior->fixed_values().q();
+
+
+        
+        // Check validity
+        if (p != hypers->beta.rows()) {
+            throw std::invalid_argument(
+                "Hyperparameters dimensions are not consistent");
+        }
+        
+        if (hypers->beta <= 0) {
+            throw std::invalid_argument("Shape parameter must be > 0");
+        }
+        if (hypers->alpha0 <= 0) {
+            throw std::invalid_argument("Scale parameter must be > 0");
+        }
+        if (phi->beta <= 0) {
+            throw std::invalid_argument("Diffusion parameter must be > 0");
+        }
+        if (phi->q <= 0) {
+            throw std::invalid_argument("Number of factors must be > 0");
+        }
+    }
+
+    else {
+        throw std::invalid_argument("Unrecognized hierarchy prior");
+    }
+}
+
+void MFAHierarchy::update_hypers(
+    const std::vector<bayesmix::AlgorithmState::ClusterState>& states) {
+    auto& rng = bayesmix::Rng::Instance().get();
+    if (prior->has_fixed_values()) {
+        return;
+    }
+
+    else {
+        throw std::invalid_argument("Unrecognized hierarchy prior");
+    }
+}
+
+void MFAHierarchy::update_summary_statistics(const Eigen::RowVectorXd& datum,
+    bool add) {
+    if (add) {
+        data_sum += datum;
+        data_sum_squares += datum .cwiseProduct(datum);
+        data.push_back(datum);
+    }
+    else {
+        data_sum -= datum;
+        data_sum_squares -= datum.cwiseProduct(datum);
+        auto pos = std::find(data.begin(), data.end(), datum)
+        data.erase(pos);
+    }
+}
+
+void MFAHierarchy::clear_summary_statistics() {
+    data_sum = Eigen::VectorXd::Zero(p);
+    data_sum_squares = Eigen::VectorXd::Zero(p);
+    data.clear();
+}
+
+void MFAHierarchy::set_state_from_proto(
+    const google::protobuf::Message& state_) {
+    auto& statecast = downcast_state(state_);
+    state.mu = statecast.mfa_state().mu(); // TODO add, ls??
+    state.psi = statecast.mfa_state().psi();
+    state.eta = statecast.mfa_state().eta();
+    state.lambda = statecast.mfa_state().lambda();
+    set_card(statecast.cardinality());
+}
+
+std::shared_ptr<bayesmix::AlgorithmState::ClusterState>
+MFAHierarchy::get_state_proto() const {
+    bayesmix::MFAState state_;
+    state_.set_mu(state.mu);
+    state_.set_psi(state.psi);
+    state_.set_Eta(state.Eta);
+    state_.set_Lambda(state.Lambda);
+
+    auto out = std::make_shared<bayesmix::AlgorithmState::ClusterState>();
+    out->mutable_mfa_state()->CopyFrom(state_);
+    return out;
+}
+
+void MFAHierarchy::set_hypers_from_proto(
+    const google::protobuf::Message& hypers_) {
+    auto& hyperscast = downcast_hypers(hypers_).mfa_state();
+    hypers->mutilde = hyperscast.mutilde();
+    hypers->alpha0 = hyperscast.alpha0();
+    hypers->beta = hyperscast.beta();
+    hypers->phi = hyperscast.phi();
+}
+
+std::shared_ptr<bayesmix::AlgorithmState::HierarchyHypers>
+MFAHierarchy::get_hypers_proto() const {
+    bayesmix::MFAState hypers_;
+    hypers_.set_mutilde(hypers->mutilde);
+    hypers_.set_alpha0(hypers->alpha0);
+    hypers_.set_beta(hypers->beta);
+    hypers_.set_phi(hypers->phi);
+
+    auto out = std::make_shared<bayesmix::AlgorithmState::HierarchyHypers>();
+    out->mutable_mfa_state()->CopyFrom(hypers_);
+    return out;
+}
+
+void MFAHierarchy::sample_full_cond(bool update_params = true) override {
+    if (this->card == 0) {
+        // No posterior update possible
+        static_cast<Derived*>(this)->sample_prior();
+    }
+    else {
+        std::shared_ptr<State> newstate = nullptr;
+
+        if (update_params)
+            newstate = &state;
+        else
+            newstate = new State;
+
+        sample_Eta(*newstate);
+
+        sample_mu(*newstate);
+
+        sample_psi(*newstate);
+
+        sample_Lambda(*newstate);
+
+        if (!update_params)
+            state = *newstate;
+    }
+
+}
+
+void MFAHierarchy::sample_Eta(State &newstate) const {
+
+    auto& rng = bayesmix::Rng::Instance().get();
+
+    Eigen::MatrixXd Sigmaeta = (Eigen::MatrixXd::Identity(q, q) + state.Lambda.transpose() * Eigen::MatrixXd(psi.cwiseInverse()).asDiagonal() * state.Lambda).inverse();
+
+    for (size_t i = 0; i < card; i++) {
+        newstate.Eta.row(i) = stan::math::multi_normal_rng(Sigmaeta * state.Lambda.transpose() * Eigen::MatrixXd(psi.cwiseInverse()).asDiagonal() * (data[i] - state.mu), Sigmaeta, rng);
+}
+
+}
+
+void MFAHierarchy::sample_mu(State &newstate) const {
+
+    auto& rng = bayesmix::Rng::Instance().get();
+
+    Eigen::MatrixXd Sigmamu = (hypers->phi*Eigen::MatrixXd::Identity(p, p) + card * Eigen::MatrixXd(psi.cwiseInverse()).asDiagonal()).inverse();
+
+    Eigen::VectorXd Somma = Eigen::VectorXd::Zero(p);
+
+    for (size_t i = 0; i < card; i++) {
+        Somma += state.Lambda * state.Eta.row(i);
+    }
+    
+    newstate.mu = stan::math::multi_normal_rng(Sigmamu *(hypers->phi * hypers->mutilde + Eigen::MatrixXd(psi.cwiseInverse()).asDiagonal()*(data_sum-Somma)), Sigmamu, rng);
+    
+
+}
+
+void MFAHierarchy::sample_Lambda(State &newstate) const {
+
+    auto& rng = bayesmix::Rng::Instance().get();
+
+
+    for (size_t j = 0; j < p; j++) {
+
+        Eigen::MatrixXd Sigmalambda = (Eigen::MatrixXd::Identity(q, q) + state.Eta.transpose()/ state.psi[j] * state.Eta).inverse();
+
+        newstate.Lambda.row(j) = stan::math::multi_normal_rng(Sigmalambda * state.Eta.transpose() / state.psi[j] * (data.col(j) - state.mu[j]), Sigmalambda, rng);
+    }
+
+}
+
+void MFAHierarchy::sample_psi(State &newstate) const {
+
+    auto& rng = bayesmix::Rng::Instance().get();
+
+
+    for (size_t j = 0; j < p; j++) {
+        
+        double S=0;
+        for (size_t i = 0; i < card; i++) {
+            S += std::pow((data[i, j] - state.mu[j] - state.Lambda.row(j).dot(state.Eta.row(i))),2);
+        }
+        newstate.psi[j] = stan::math::inv_gamma_rng(hypers->alpha0 + card/2,hypers->beta[j] + S/2, rng);
+    }
+
+}
---
+++ src/hierarchies/mfa_hierarchy.h
@@ -0,0 +1,123 @@
+#ifndef BAYESMIX_HIERARCHIES_MFA_HIERARCHY_H_
+#define BAYESMIX_HIERARCHIES_MFA_HIERARCHY_H_
+
+#include <google/protobuf/stubs/casts.h>
+
+#include <Eigen/Dense>
+#include <memory>
+#include <vector>
+
+#include "algorithm_state.pb.h"
+#include "base_hierarchy.h"
+#include "hierarchy_id.pb.h"
+#include "hierarchy_prior.pb.h"
+
+//! Mixture of Factor Analysers hierarchy for multivariate data.
+
+//! This class represents a hierarchical model where data are distributed
+
+namespace MFA {
+//! Custom container for State values
+struct State {
+  Eigen::VectorXd mu, psi;
+  Eigen::MatrixXd Eta, Lambda;
+};
+
+//! Custom container for Hyperparameters values
+struct Hyperparams {
+  Eigen::VectorXd mutilde, beta;
+  double phi, alpha0;
+  unsigned int q;
+};
+
+};  // namespace MFA
+
+class MFAHierarchy
+    : public BaseHierarchy<MFAHierarchy, MFA::State, MFA::Hyperparams,
+                           bayesmix::MFAPrior> {
+ public:
+  MFAHierarchy() = default;
+  ~MFAHierarchy() = default;
+
+  //! Updates hyperparameter values given a vector of cluster states
+  void update_hypers(const std::vector<bayesmix::AlgorithmState::ClusterState>&
+                         states) override;
+
+  //! Updates state values using the given (prior or posterior) hyperparameters
+  MFA::State draw(const MFA::Hyperparams& params);
+
+  //! Resets summary statistics for this cluster
+  void clear_summary_statistics() override;
+
+  //! Returns the Protobuf ID associated to this class
+  bayesmix::HierarchyId get_id() const override {
+    return bayesmix::HierarchyId::MFA;
+  }
+
+  //! Read and set state values from a given Protobuf message
+  void set_state_from_proto(const google::protobuf::Message& state_) override;
+
+  //! Read and set hyperparameter values from a given Protobuf message
+  void set_hypers_from_proto(
+      const google::protobuf::Message& hypers_) override;
+
+  //! Writes current state to a Protobuf message and return a shared_ptr
+  //! New hierarchies have to first modify the field 'oneof val' in the
+  //! AlgoritmState::ClusterState message by adding the appropriate type
+  std::shared_ptr<bayesmix::AlgorithmState::ClusterState> get_state_proto()
+      const override;
+
+  //! Writes current value of hyperparameters to a Protobuf message and
+  //! return a shared_ptr.
+  //! New hierarchies have to first modify the field 'oneof val' in the
+  //! AlgoritmState::HierarchyHypers message by adding the appropriate type
+  std::shared_ptr<bayesmix::AlgorithmState::HierarchyHypers> get_hypers_proto()
+      const override;
+
+  //! Returns whether the hierarchy models multivariate data or not
+  bool is_multivariate() const override { return true; }
+
+ protected:
+  //! Evaluates the log-likelihood of data in a single point
+  //! @param datum      Point which is to be evaluated
+  //! @return           The evaluation of the lpdf
+  double like_lpdf(const Eigen::RowVectorXd& datum) const override;
+
+  //! Updates cluster statistics when a datum is added or removed from it
+  //! @param datum      Data point which is being added or removed
+  //! @param add        Whether the datum is being added or removed
+  void update_summary_statistics(const Eigen::RowVectorXd& datum,
+                                 bool add) override;
+
+  //! Initializes state parameters to appropriate values
+  void initialize_state() override;
+
+  //! Initializes hierarchy hyperparameters to appropriate values
+  void initialize_hypers() override;
+
+  //! Generates new state values from the centering posterior distribution
+  //! @param update_params  Save posterior hypers after the computation?
+  void sample_full_cond(bool update_params = true) override;
+
+  void sample_Eta(State& newstate) const;
+
+  void sample_mu(State& newstate) const;
+
+  void sample_psi(State& newstate) const;
+
+  void sample_Lambda(State& newstate) const;
+
+  //! Sum of data points currently belonging to the cluster
+  Eigen::VectorXd data_sum = 0;
+
+  //! Sum of squared data points currently belonging to the cluster
+  Eigen::VectorXd data_sum_squares = 0;
+
+  //! Number of initial variables
+  size_t p;
+
+  //! Deque of data points currently belonging to the cluster
+  std::deque<Eigen::RowVectorXd> data;
+};
+
+#endif  // BAYESMIX_HIERARCHIES_MFA_HIERARCHY_H_
