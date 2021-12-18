52fb8a8390e8a20dfdd44af870fa3c348809d257
elucasticus <luca7.crippa@mail.polimi.it>
2021-12-18 11:03:04 +0100

    add structure to test code

---
+++ examples/examples_mfa_hierarchy/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# build/run \
+#   algorithm_settings_file \
+#   hierarchy_type  hierarchy_prior_file \
+#   mixing_type  mixing_prior_file \
+#   collector_name \
+#   data_file \
+#   grid_file \
+#   density_output_file \
+#   numclust_output_file \
+#   clustering_output_file \
+#   [hierarchy_covariates_file] \
+#   [hierarchy_grid_covariates_file] \
+#   [mixing_covariates_file] \
+#   [mixing_grid_covariates_file]
+
+build/run \
+  examples/examples_mfa_hierarchy/in/algo.asciipb \
+  NNIG examples/examples_mfa_hierarchy/in/nnig_ngg.asciipb \
+  DP   examples/examples_mfa_hierarchy/in/dp_gamma.asciipb \
+  examples/examples_mfa_hierarchy/out/chains.recordio \
+  examples/examples_mfa_hierarchy/in/data.csv \
+  examples/examples_mfa_hierarchy/in/grid.csv \
+  examples/examples_mfa_hierarchy/out/density_file.csv \
+  examples/examples_mfa_hierarchy/out/numclust.csv \
+  examples/examples_mfa_hierarchy/out/clustering.csv
