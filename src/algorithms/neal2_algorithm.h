2bc11c68136f02028bcc565223414458b8fa8ad9
giacomodecarlo <80590505+giacomodecarlo@users.noreply.github.com>
2021-12-23 16:41:08 +0100

    Neal2 is Conjugate

--- src/algorithms/neal2_algorithm.h
+++ src/algorithms/neal2_algorithm.h
@@ -20,7 +20,7 @@ class Neal2Algorithm : public MarginalAlgorithm {
   Neal2Algorithm() = default;
   ~Neal2Algorithm() = default;
 
-  bool requires_conjugate_hierarchy() const override { return false; }
+  bool requires_conjugate_hierarchy() const override { return true; }
 
   bayesmix::AlgorithmId get_id() const override {
     return bayesmix::AlgorithmId::Neal2;
