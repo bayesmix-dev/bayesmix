f5f8400122de4db95bdbdd82971a07a8a21f8da5
Matteo Gianella <gianeteo@hotmail.it>
2021-12-27 21:19:31 +0100

    Added NNxIG hierarchy

---
+++ src/hierarchies/nnxig_hierarchy.h
@@ -0,0 +1,120 @@
+#ifndef BAYESMIX_HIERARCHIES_NNXIG_HIERARCHY_H_
+#define BAYESMIX_HIERARCHIES_NNXIG_HIERARCHY_H_
+
+#include <google/protobuf/stubs/casts.h>
+
+#include <Eigen/Dense>
+#include <memory>
+#include <vector>
+
+#include "algorithm_state.pb.h"
+#include "base_hierarchy.h"
+#include "hierarchy_id.pb.h"
+#include "hierarchy_prior.pb.h"
+
+//! Non Conjugate Normal Normal-InverseGamma hierarchy for univariate data.
+
+//! This class represents a hierarchical model where data are distributed
+//! according to a normal likelihood, the parameters of which have a
+//! Normal-InverseGamma centering distribution. That is:
+//! f(x_i|mu,sig) = N(mu,sig^2)
+//!            mu ~ N(mu0, sigma0)
+//!         sig^2 ~ IG(alpha0, beta0)
+//! The state is composed of mean and variance. The state hyperparameters,
+//! contained in the Hypers object, are (mu0, sigma0, alpha0, beta0), all
+//! scalar values. Note that this hierarchy is non conjugate.
+
+namespace NNxIG {
+//! Custom container for State values
+struct State {
+  double mean, var;
+};
+
+//! Custom container for Hyperparameters values
+struct Hyperparams {
+  double mean, var, shape, scale;
+};
+
+};  // namespace NNxIG
+
+class NNxIGHierarchy
+    : public BaseHierarchy<NNxIGHierarchy, NNxIG::State, NNxIG::Hyperparams,
+                           bayesmix::NNxIGPrior> {
+ public:
+  NNxIGHierarchy() = default;
+  ~NNxIGHierarchy() = default;
+
+  //! Updates hyperparameter values given a vector of cluster states
+  void update_hypers(const std::vector<bayesmix::AlgorithmState::ClusterState>
+                         &states) override;
+
+  //! Updates state values using the given (prior or posterior) hyperparameters
+  NNxIG::State draw(const NNxIG::Hyperparams &params);
+
+  //! Generates new state values from the centering posterior distribution
+  //! @param update_params  Save posterior hypers after the computation?
+  void sample_full_cond(bool update_params = true) override;
+
+  //! Saves posterior hyperparameters to the corresponding class member
+  void save_posterior_hypers();
+
+  //! Resets summary statistics for this cluster
+  void clear_summary_statistics() override;
+
+  //! Returns the Protobuf ID associated to this class
+  bayesmix::HierarchyId get_id() const override {
+    return bayesmix::HierarchyId::NNxIG;
+  }
+
+  //! Read and set state values from a given Protobuf message
+  void set_state_from_proto(const google::protobuf::Message &state_) override;
+
+  //! Read and set hyperparameter values from a given Protobuf message
+  void set_hypers_from_proto(
+      const google::protobuf::Message &hypers_) override;
+
+  //! Writes current state to a Protobuf message and return a shared_ptr
+  //! New hierarchies have to first modify the field 'oneof val' in the
+  //! AlgoritmState::ClusterState message by adding the appropriate type
+  std::shared_ptr<bayesmix::AlgorithmState::ClusterState> get_state_proto()
+      const override;
+
+  //! Writes current value of hyperparameters to a Protobuf message and
+  //! return a shared_ptr.
+  //! New hierarchies have to first modify the field 'oneof val' in the
+  //! AlgoritmState::HierarchyHypers message by adding the appropriate type
+  std::shared_ptr<bayesmix::AlgorithmState::HierarchyHypers> get_hypers_proto()
+      const override;
+
+  //! Computes and return posterior hypers given data currently in this cluster
+  NNxIG::Hyperparams compute_posterior_hypers() const;
+
+  //! Returns whether the hierarchy models multivariate data or not
+  bool is_multivariate() const override { return false; }
+
+ protected:
+  //! Evaluates the log-likelihood of data in a single point
+  //! @param datum      Point which is to be evaluated
+  //! @return           The evaluation of the lpdf
+  double like_lpdf(const Eigen::RowVectorXd &datum) const override;
+
+  //! Updates cluster statistics when a datum is added or removed from it
+  //! @param datum      Data point which is being added or removed
+  //! @param add        Whether the datum is being added or removed
+  void update_summary_statistics(const Eigen::RowVectorXd &datum,
+                                 bool add) override;
+
+  //! Initializes state parameters to appropriate values
+  void initialize_state() override;
+
+  //! Initializes hierarchy hyperparameters to appropriate values
+  void initialize_hypers() override;
+
+  //! Sum of data points currently belonging to the cluster
+  double data_sum = 0;
+
+  //! Sum of squared data points currently belonging to the cluster
+  double data_sum_squares = 0;
+};
+
+#endif  // BAYESMIX_HIERARCHIES_NNXIG_HIERARCHY_H_
