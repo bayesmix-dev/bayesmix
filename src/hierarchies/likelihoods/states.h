87e52cb9d77a3f0f49f6e55560e640227cd390a5
Matteo Gianella <gianeteo@hotmail.it>
2022-01-11 17:02:52 +0100

    Definition of currently implementes states

---
+++ src/hierarchies/likelihoods/states.h
@@ -0,0 +1,25 @@
+#ifndef BAYESMIX_HIERARCHIES_LIKELIHOOD_STATES_H_
+#define BAYESMIX_HIERARCHIES_LIKELIHOOD_STATES_H_
+
+#include <Eigen/Dense>
+
+namespace State {
+
+struct UniLS {
+  double mean, var;
+};
+
+struct MultiLS {
+  Eigen::VectorXd mean;
+  Eigen::MatrixXd prec, prec_chol;
+  double prec_logdet;
+};
+
+struct UniLinReg {
+  Eigen::VectorXd regression_coeffs;
+  double var;
+};
+
+}  // namespace State
+
+#endif
