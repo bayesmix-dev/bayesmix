ad65aec81b5e54b86e91a1816387b318891a65a1
Matteo Gianella <gianeteo@hotmail.it>
2022-01-11 17:04:36 +0100

    Definitions of currently implemented hyper-parameters structs

---
+++ src/hierarchies/priors/hyperparams.h
@@ -0,0 +1,30 @@
+#ifndef BAYESMIX_HIERARCHIES_PRIORMODEL_HYPERPARAMS_H_
+#define BAYESMIX_HIERARCHIES_PRIORMODEL_HYPERPARAMS_H_
+
+#include <Eigen/Dense>
+
+namespace Hyperparams {
+
+struct NIG {
+  double mean, var_scaling, shape, scale;
+};
+
+struct NxIG {
+  double mean, var, shape, scale;
+};
+
+struct NW {
+  Eigen::VectorXd mean;
+  double var_scaling, deg_free;
+  Eigen::MatrixXd scale, scale_inv, scale_chol;
+};
+
+struct MNIG {
+  Eigen::VectorXd mean;
+  Eigen::MatrixXd var_scaling, var_scaling_inv;
+  double shape, scale;
+};
+
+}  // namespace Hyperparams
+
+#endif
