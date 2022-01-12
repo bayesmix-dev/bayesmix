5fe9bf254ac3d0e33d667a7dbbd70a37eb791b6f
Matteo Gianella <gianeteo@hotmail.it>
2022-01-12 17:41:22 +0100

    Move methods' definitions to source file

---
+++ src/hierarchies/likelihoods/uni_norm_likelihood.cc
@@ -0,0 +1,39 @@
+#include "uni_norm_likelihood.h"
+
+double UniNormLikelihood::compute_lpdf(const Eigen::RowVectorXd &datum) const {
+  return stan::math::normal_lpdf(datum(0), state.mean, sqrt(state.var));
+}
+
+void UniNormLikelihood::update_summary_statistics(
+    const Eigen::RowVectorXd &datum, bool add) {
+  if (add) {
+    data_sum += datum(0);
+    data_sum_squares += datum(0) * datum(0);
+  } else {
+    data_sum -= datum(0);
+    data_sum_squares -= datum(0) * datum(0);
+  }
+}
+
+void UniNormLikelihood::set_state_from_proto(
+    const google::protobuf::Message &state_) {
+  auto &statecast = downcast_state(state_);
+  state.mean = statecast.uni_ls_state().mean();
+  state.var = statecast.uni_ls_state().var();
+  set_card(statecast.cardinality());
+}
+
+std::shared_ptr<bayesmix::AlgorithmState::ClusterState>
+UniNormLikelihood::get_state_proto() const {
+  bayesmix::UniLSState state_;
+  state_.set_mean(state.mean);
+  state_.set_var(state.var);
+  auto out = std::make_shared<bayesmix::AlgorithmState::ClusterState>();
+  out->mutable_uni_ls_state()->CopyFrom(state_);
+  return out;
+}
+
+void UniNormLikelihood::clear_summary_statistics() {
+  data_sum = 0;
+  data_sum_squares = 0;
+}
--- src/hierarchies/likelihoods/uni_norm_likelihood.h
+++ src/hierarchies/likelihoods/uni_norm_likelihood.h
@@ -5,6 +5,7 @@
 
 #include <Eigen/Dense>
 #include <memory>
+#include <stan/math/prim.hpp>
 #include <vector>
 
 #include "algorithm_state.pb.h"
@@ -13,41 +14,23 @@
 
 class UniNormLikelihood
     : public BaseLikelihood<UniNormLikelihood, State::UniLS> {
- private:
-  double data_sum = 0;
-  double data_sum_squares = 0;
-
  public:
   UniNormLikelihood() = default;
-
   ~UniNormLikelihood() = default;
-
   bool is_multivariate() const override { return false; };
-
   bool is_dependent() const override { return false; };
-
-  void set_state_from_proto(const google::protobuf::Message &state_) override {
-    auto &statecast = downcast_state(state_);
-    state.mean = statecast.uni_ls_state().mean();
-    state.var = statecast.uni_ls_state().var();
-    set_card(statecast.cardinality());
-  };
-
+  void set_state_from_proto(const google::protobuf::Message &state_) override;
   std::shared_ptr<bayesmix::AlgorithmState::ClusterState> get_state_proto()
-      const override {
-    bayesmix::UniLSState state_;
-    state_.set_mean(state.mean);
-    state_.set_var(state.var);
+      const override;
+  void clear_summary_statistics() override;
 
-    auto out = std::make_shared<bayesmix::AlgorithmState::ClusterState>();
-    out->mutable_uni_ls_state()->CopyFrom(state_);
-    return out;
-  };
+ protected:
+  double compute_lpdf(const Eigen::RowVectorXd &datum) const override;
+  void update_summary_statistics(const Eigen::RowVectorXd &datum,
+                                 bool add) override;
 
-  void clear_summary_statistics() override {
-    data_sum = 0;
-    data_sum_squares = 0;
-  }
+  double data_sum = 0;
+  double data_sum_squares = 0;
 };
 
 #endif
