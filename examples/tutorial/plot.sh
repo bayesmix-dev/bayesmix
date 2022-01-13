f9e35941e8de017820ecb1a0ab724b9e8204d642
brunoguindani <bruno@guindani.org>
2022-01-13 10:31:08 +0100

    density_file.csv -> density.csv

--- examples/tutorial/plot.sh
+++ examples/tutorial/plot.sh
@@ -2,7 +2,7 @@
 
 build/plots/plot_mcmc \
   --grid-file resources/tutorial/grid.csv \
-  --dens-file resources/tutorial/out/density_file.csv \
+  --dens-file resources/tutorial/out/density.csv \
   --n-cl-file resources/tutorial/out/numclust.csv \
   --dens-plot resources/tutorial/out/density.png \
   --n-cl-trace-plot resources/tutorial/out/traceplot.png \
