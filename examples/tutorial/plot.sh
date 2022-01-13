a8570d1909870f8bf9d9cc9f4625247e7b60ac28
brunoguindani <bruno@guindani.org>
2022-01-13 10:57:56 +0100

    Rearranged plot.sh args

--- examples/tutorial/plot.sh
+++ examples/tutorial/plot.sh
@@ -3,7 +3,7 @@
 build/plots/plot_mcmc \
   --grid-file resources/tutorial/grid.csv \
   --dens-file resources/tutorial/out/density.csv \
-  --n-cl-file resources/tutorial/out/numclust.csv \
   --dens-plot resources/tutorial/out/density.png \
+  --n-cl-file resources/tutorial/out/numclust.csv \
   --n-cl-trace-plot resources/tutorial/out/traceplot.png \
   --n-cl-hist-plot  resources/tutorial/out/histogram.png
