cb14d176419934c9beac9a2bcdbbb62cf5fbe99d
Matteo Gianella <gianeteo@hotmail.it>
2022-01-14 18:32:42 +0100

    Add NIGPriorModel test with Normal Mean Prior

--- test/prior_models.cc
+++ test/prior_models.cc
@@ -34,21 +34,23 @@ TEST(nig_prior_model, set_get_hypers) {
   ASSERT_EQ(got_state_.DebugString(), set_state_.DebugString());
 }
 
-// TODO: test for the other priors available
 TEST(nig_prior_model, fixed_values_prior) {
+  // Prepare buffers
   bayesmix::NNIGPrior prior;
   bayesmix::AlgorithmState::HierarchyHypers prior_out;
+  std::vector<std::shared_ptr<AbstractPriorModel>> prior_models;
+  std::vector<bayesmix::AlgorithmState::ClusterState> states;
+
+  // Set fixed value prior
   prior.mutable_fixed_values()->set_mean(5.0);
   prior.mutable_fixed_values()->set_var_scaling(0.1);
   prior.mutable_fixed_values()->set_shape(2.0);
   prior.mutable_fixed_values()->set_scale(2.0);
 
+  // Initialize prior model
   auto prior_model = std::make_shared<NIGPriorModel>();
   prior_model->get_mutable_prior()->CopyFrom(prior);
   prior_model->initialize();
-
-  std::vector<std::shared_ptr<AbstractPriorModel>> prior_models;
-  std::vector<bayesmix::AlgorithmState::ClusterState> states;
 
   // Check equality before update
   prior_models.push_back(prior_model);
@@ -67,4 +69,39 @@ TEST(nig_prior_model, fixed_values_prior) {
     ASSERT_EQ(prior.fixed_values().DebugString(),
               prior_out.nnig_state().DebugString());
   }
+}
+
+TEST(nig_prior_model, normal_mean_prior) {
+  // Prepare buffers
+  bayesmix::NNIGPrior prior;
+  bayesmix::AlgorithmState::HierarchyHypers prior_out;
+
+  // Set Normal prior on the mean
+  double mu00 = 0.5;
+  prior.mutable_normal_mean_prior()->mutable_mean_prior()->set_mean(mu00);
+  prior.mutable_normal_mean_prior()->mutable_mean_prior()->set_var(1.02);
+  prior.mutable_normal_mean_prior()->set_var_scaling(0.1);
+  prior.mutable_normal_mean_prior()->set_shape(2.0);
+  prior.mutable_normal_mean_prior()->set_scale(2.0);
+
+  // Prepare some fictional states
+  std::vector<bayesmix::AlgorithmState::ClusterState> states(4);
+  for (int i = 0; i < states.size(); i++) {
+    double mean = 9.0 + i;
+    states[i].mutable_uni_ls_state()->set_mean(mean);
+    states[i].mutable_uni_ls_state()->set_var(1.0);
+  }
+
+  // Initialize prior model
+  auto prior_model = std::make_shared<NIGPriorModel>();
+  prior_model->get_mutable_prior()->CopyFrom(prior);
+  prior_model->initialize();
+
+  // Update hypers in light of current states
+  prior_model->update_hypers(states);
+  prior_model->write_hypers_to_proto(&prior_out);
+  double mean_out = prior_out.nnig_state().mean();
+
+  // Check
+  ASSERT_GT(mean_out, mu00);
 }
