e3fa06d1ed685871b8db43970d6b6793e4eed200
Matteo Gianella <gianeteo@hotmail.it>
2022-01-14 14:56:13 +0100

    Add unit tests for NIGPriorModel

--- test/prior_models.cc
+++ test/prior_models.cc
@@ -33,3 +33,38 @@ TEST(nig_prior_model, set_get_hypers) {
   // Check if they coincides
   ASSERT_EQ(got_state_.DebugString(), set_state_.DebugString());
 }
+
+// TODO: test for the other priors available
+TEST(nig_prior_model, fixed_values_prior) {
+  bayesmix::NNIGPrior prior;
+  bayesmix::AlgorithmState::HierarchyHypers prior_out;
+  prior.mutable_fixed_values()->set_mean(5.0);
+  prior.mutable_fixed_values()->set_var_scaling(0.1);
+  prior.mutable_fixed_values()->set_shape(2.0);
+  prior.mutable_fixed_values()->set_scale(2.0);
+
+  auto prior_model = std::make_shared<NIGPriorModel>();
+  prior_model->get_mutable_prior()->CopyFrom(prior);
+  prior_model->initialize();
+
+  std::vector<std::shared_ptr<AbstractPriorModel>> prior_models;
+  std::vector<bayesmix::AlgorithmState::ClusterState> states;
+
+  // Check equality before update
+  prior_models.push_back(prior_model);
+  for (size_t i = 1; i < 4; i++) {
+    prior_models.push_back(prior_model->clone());
+    prior_models[i]->write_hypers_to_proto(&prior_out);
+    ASSERT_EQ(prior.fixed_values().DebugString(),
+              prior_out.nnig_state().DebugString());
+  }
+
+  // Check equality after update
+  prior_models[0]->update_hypers(states);
+  prior_models[0]->write_hypers_to_proto(&prior_out);
+  for (size_t i = 1; i < 4; i++) {
+    prior_models[i]->write_hypers_to_proto(&prior_out);
+    ASSERT_EQ(prior.fixed_values().DebugString(),
+              prior_out.nnig_state().DebugString());
+  }
+}
