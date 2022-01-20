858671f40c006ac628d2c77c49ec722aa1c83875
Matteo Gianella <gianeteo@hotmail.it>
2022-01-20 22:28:44 +0100

    Comment-out non implemented hierarchies

--- src/hierarchies/load_hierarchies.h
+++ src/hierarchies/load_hierarchies.h
@@ -6,10 +6,10 @@
 
 #include "abstract_hierarchy.h"
 #include "hierarchy_id.pb.h"
-#include "lin_reg_uni_hierarchy.h"
+// #include "lin_reg_uni_hierarchy.h"
 #include "nnig_hierarchy.h"
-#include "nnw_hierarchy.h"
-#include "nnxig_hierarchy.h"
+// #include "nnw_hierarchy.h"
+// #include "nnxig_hierarchy.h"
 #include "src/runtime/factory.h"
 
 //! Loads all available `Hierarchy` objects into the appropriate factory, so
@@ -26,20 +26,20 @@ __attribute__((constructor)) static void load_hierarchies() {
   Builder<AbstractHierarchy> NNIGbuilder = []() {
     return std::make_shared<NNIGHierarchy>();
   };
-  Builder<AbstractHierarchy> NNxIGbuilder = []() {
-    return std::make_shared<NNxIGHierarchy>();
-  };
-  Builder<AbstractHierarchy> NNWbuilder = []() {
-    return std::make_shared<NNWHierarchy>();
-  };
-  Builder<AbstractHierarchy> LinRegUnibuilder = []() {
-    return std::make_shared<LinRegUniHierarchy>();
-  };
+  // Builder<AbstractHierarchy> NNxIGbuilder = []() {
+  //   return std::make_shared<NNxIGHierarchy>();
+  // };
+  // Builder<AbstractHierarchy> NNWbuilder = []() {
+  //   return std::make_shared<NNWHierarchy>();
+  // };
+  // Builder<AbstractHierarchy> LinRegUnibuilder = []() {
+  //   return std::make_shared<LinRegUniHierarchy>();
+  // };
 
   factory.add_builder(NNIGHierarchy().get_id(), NNIGbuilder);
-  factory.add_builder(NNxIGHierarchy().get_id(), NNxIGbuilder);
-  factory.add_builder(NNWHierarchy().get_id(), NNWbuilder);
-  factory.add_builder(LinRegUniHierarchy().get_id(), LinRegUnibuilder);
+  // factory.add_builder(NNxIGHierarchy().get_id(), NNxIGbuilder);
+  // factory.add_builder(NNWHierarchy().get_id(), NNWbuilder);
+  // factory.add_builder(LinRegUniHierarchy().get_id(), LinRegUnibuilder);
 }
 
 #endif  // BAYESMIX_HIERARCHIES_LOAD_HIERARCHIES_H_
