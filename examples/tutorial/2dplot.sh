d5ed8afffc8a4ff0f47bee8e8c2687724b44d9cb
brunoguindani <bruno@guindani.org>
2022-01-31 22:21:51 +0100

    Corrected 2dplot.sh

--- examples/tutorial/2dplot.sh
+++ examples/tutorial/2dplot.sh
@@ -3,7 +3,7 @@
 build/plots/plot_mcmc \
   --grid-file resources/datasets/faithful_grid.csv \
   --dens-file resources/2d/density_2d.csv \
-  --dens-plot resources/tutorial/out/density.png \
+  --dens-plot resources/2d/density.png \
   --n-cl-file resources/2d/numclust_2d.csv \
   --n-cl-trace-plot resources/2d/traceplot.png \
   --n-cl-hist-plot  resources/2d/histogram.png
