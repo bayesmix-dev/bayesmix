575a85350280b9f27878136066e8ab6c3ee81344
Mario beraha <berahamario@gmail.com>
2022-02-03 10:21:40 +0100

    merged master

--- examples/tutorial/2drun.sh
+++ examples/tutorial/2drun.sh
@@ -10,3 +10,6 @@ build/run_mcmc \
   --dens-file resources/2d/density_2d.csv \
   --n-cl-file resources/2d/numclust_2d.csv \
   --clus-file resources/2d/clustering_2d.csv
+
+
+run --algo-params-file resources/tutorial/algo.asciipb --hier-type NNW --hier-args resources/tutorial/nnw_ngiw.asciipb --mix-type DP --mix-args resources/tutorial/dp_gamma.asciipb --data-file resources/datasets/faithful.csv --grid-file resources/datasets/faithful_grid.csv --coll-name memory --dens-file resources/2d/density_2d.csv --n-cl-file resources/2d/numclust_2d.csv --clus-file resources/2d/clustering_2d.csv
