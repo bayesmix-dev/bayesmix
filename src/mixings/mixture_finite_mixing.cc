8e78c5859baf87189cea76279cb8a5c6d32e5162
Enrico <enrico.paglia@mail.polimi.it>
2022-02-05 17:24:48 +0100

    clang style

--- src/mixings/mixture_finite_mixing.cc
+++ src/mixings/mixture_finite_mixing.cc
@@ -29,12 +29,12 @@ double MixtureFiniteMixing::mass_existing_cluster(
     const unsigned int n, const bool log, const bool propto,
     std::shared_ptr<AbstractHierarchy> hier,
     const unsigned int n_clust) const {
-
   if (!V_C_are_initialized) {
     init_V_C(n + 2);
   }
 
-  // If prop to is false we will need V[n_clust] and V[n_clust+1] to compute the normalizing constant
+  // If prop to is false we will need V[n_clust] and V[n_clust+1] to compute
+  // the normalizing constant
   if (!propto) {
     if (V[n_clust + 1] < 0) {
       compute_V_t(n_clust + 1, n + 1);
@@ -54,8 +54,8 @@ double MixtureFiniteMixing::mass_existing_cluster(
   } else {
     out = hier->get_card() + state.gamma;
     if (!propto) {
-      out = out /
-            (n + n_clust * state.gamma + (V[n_clust + 1] / V[n_clust] * state.gamma));
+      out = out / (n + n_clust * state.gamma +
+                   (V[n_clust + 1] / V[n_clust] * state.gamma));
     }
   }
   return out;
@@ -64,7 +64,6 @@ double MixtureFiniteMixing::mass_existing_cluster(
 double MixtureFiniteMixing::mass_new_cluster(
     const unsigned int n, const bool log, const bool propto,
     const unsigned int n_clust) const {
-
   if (!V_C_are_initialized) {
     init_V_C(n + 2);
   }
@@ -86,8 +85,8 @@ double MixtureFiniteMixing::mass_new_cluster(
   } else {
     out = V[n_clust + 1] / V[n_clust] * state.gamma;
     if (!propto) {
-      out = out /
-            (n + n_clust * state.gamma + V[n_clust + 1] / V[n_clust] * state.gamma);
+      out = out / (n + n_clust * state.gamma +
+                   V[n_clust + 1] / V[n_clust] * state.gamma);
     }
   }
   return out;
@@ -131,26 +130,26 @@ void MixtureFiniteMixing::init_V_C(unsigned int n) const {
   V = std::vector<double>(n, -1);
 
   // Compute C = log(first term of the series of V_n(0)), all the values of V
-  // that are computed are multiplied by exp(-C) to avoid ending up  with values
-  // bigger than the largest number that can be contained in a double
+  // that are computed are multiplied by exp(-C) to avoid ending up  with
+  // values bigger than the largest number that can be contained in a double
 
   double log_den = std::log(state.gamma);
   for (unsigned int i = 1; i < n; ++i) {
     log_den += std::log(state.gamma * 1 + i);
   }
-  C = - log_den + stan::math::poisson_lpmf(1, state.lambda);
+  C = -log_den + stan::math::poisson_lpmf(1, state.lambda);
 
   V_C_are_initialized = true;
 }
 
 void MixtureFiniteMixing::compute_V_t(double t, unsigned int n) const {
-
   double sum = 0;
   unsigned int k = 1;
-  double last_term_sum_rate = 1; // Rate between the last computed term of the series
-                                 // and the sum computed up to that point, this value
-                                 // is used to decide when to stop computing additional
-                                 // terms of the series
+  double last_term_sum_rate =
+      1;  // Rate between the last computed term of the series
+          // and the sum computed up to that point, this value
+          // is used to decide when to stop computing additional
+          // terms of the series
 
   while (last_term_sum_rate > 1e-8) {
     double log_num = std::log(k);
@@ -167,12 +166,14 @@ void MixtureFiniteMixing::compute_V_t(double t, unsigned int n) const {
     if (sum == 0) {
       last_term_sum_rate = 1;
     } else {
-      last_term_sum_rate = std::exp(log_num - log_den +
-                                    stan::math::poisson_lpmf(k, state.lambda) - C) /
-                           sum;
+      last_term_sum_rate =
+          std::exp(log_num - log_den +
+                   stan::math::poisson_lpmf(k, state.lambda) - C) /
+          sum;
     }
 
-    sum += std::exp(log_num - log_den + stan::math::poisson_lpmf(k, state.lambda) - C);
+    sum += std::exp(log_num - log_den +
+                    stan::math::poisson_lpmf(k, state.lambda) - C);
     ++k;
   }
   V[t] = sum;
--- src/mixings/mixture_finite_mixing.h
+++ src/mixings/mixture_finite_mixing.h
@@ -74,12 +74,11 @@ class MixtureFiniteMixing
   void initialize_state() override;
 
  protected:
-
   //! Vector V needed to cumpute the probabilities of a new or existing cluster
   mutable std::vector<double> V{};
 
-  //! Constant that is multipied by each value of V for numerical reasons, it is
-  //! computed as the first term of the series of V_n[0].
+  //! Constant that is multipied by each value of V for numerical reasons, it
+  //! is computed as the first term of the series of V_n[0].
   mutable double C;
 
   //! Checks if V has been initialized by init_V_C
@@ -90,7 +89,6 @@ class MixtureFiniteMixing
 
   //! Computes V_n[t] and stores it in V
   void compute_V_t(double t, unsigned int n) const;
-
 };
 
 #endif  // BAYESMIX_MIXINGS_MIXTURE_FINITE_MIXTURES_H_
