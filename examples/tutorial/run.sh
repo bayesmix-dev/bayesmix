6e8168c13bbd6a58a6fa1a641e179648e1a52a38
Enrico <enrico.paglia@mail.polimi.it>
2022-02-10 19:22:20 +0100

    restore run.sh

--- examples/tutorial/run.sh
+++ examples/tutorial/run.sh
@@ -3,7 +3,7 @@
 build/run_mcmc \
   --algo-params-file resources/tutorial/algo.asciipb \
   --hier-type NNIG --hier-args resources/tutorial/nnig_ngg.asciipb \
-  --mix-type MFM --mix-args resources/tutorial/mfm_fixed.asciipb \
+  --mix-type DP --mix-args resources/tutorial/dp_gamma.asciipb \
   --coll-name resources/tutorial/out/chains.recordio \
   --data-file resources/tutorial/data.csv \
   --grid-file resources/tutorial/grid.csv \
