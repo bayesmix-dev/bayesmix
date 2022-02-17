8bc7b3654462c8af970dcb661ae07d70f60ab09d
Mario beraha <berahamario@gmail.com>
2022-02-17 09:44:44 +0100

    more moving

--- bash/push_containers.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-docker build -f docker/env/Dockerfile --platform linux/x86_64 -t mberaha/bayesmix-env .  
-docker push mberaha/bayesmix-env 
-docker build -f docker/base/Dockerfile --platform linux/x86_64 -t mberaha/bayesmix-base .
-docker push mberaha/bayesmix-base 
