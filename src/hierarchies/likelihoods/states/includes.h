d71821efcefab249abef9a45adb537fbb85e7c4a
Matteo Gianella <gianeteo@hotmail.it>
2022-03-04 22:10:57 +0100

    Change file name

--- src/hierarchies/likelihoods/states/includes.h
+++ src/hierarchies/likelihoods/states/includes.h
@@ -2,7 +2,7 @@
 #define BAYESMIX_HIERARCHIES_LIKELIHOODS_STATES_INCLUDES_H_
 
 #include "multi_ls_state.h"
-#include "uni_lin_reg_state.h"
+#include "uni_lin_reg_ls_state.h"
 #include "uni_ls_state.h"
 
 #endif  // BAYESMIX_HIERARCHIES_LIKELIHOODS_STATES_INCLUDES_H_
