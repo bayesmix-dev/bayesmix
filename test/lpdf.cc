066699ec41ecc9c483035a40a1b613e19c5d7a8c
Matteo Gianella <gianeteo@hotmail.it>
2022-03-04 22:09:02 +0100

    Uncomment old tests

--- test/lpdf.cc
+++ test/lpdf.cc
@@ -5,7 +5,7 @@
 #include <stan/math/rev.hpp>
 
 #include "algorithm_state.pb.h"
-// #include "src/hierarchies/lin_reg_uni_hierarchy.h"
+#include "src/hierarchies/lin_reg_uni_hierarchy.h"
 #include "src/hierarchies/nnig_hierarchy.h"
 // #include "src/hierarchies/nnw_hierarchy.h"
 #include "src/utils/proto_utils.h"
@@ -137,62 +137,63 @@ TEST(lpdf, nnig) {
 //   ASSERT_DOUBLE_EQ(marg, marg_murphy);
 // }
 
-// TEST(lpdf, lin_reg_uni) {
-//   // Create hierarchy objects
-//   LinRegUniHierarchy hier;
-//   bayesmix::LinRegUniPrior prior;
-//   int dim = 3;
+TEST(lpdf, lin_reg_uni) {
+  // Create hierarchy objects
+  LinRegUniHierarchy hier;
+  bayesmix::LinRegUniPrior prior;
+  int dim = 3;
 
-//   // Generate data
-//   Eigen::VectorXd datum(1);
-//   datum << 1.5;
-//   Eigen::VectorXd cov = Eigen::VectorXd::Random(dim);
+  // Generate data
+  Eigen::VectorXd datum(1);
+  datum << 1.5;
+  Eigen::VectorXd cov = Eigen::VectorXd::Random(dim);
 
-//   // Create parameters, both Eigen and proto
-//   Eigen::VectorXd mu0(dim);
-//   for (int i = 0; i < dim; i++) {
-//     mu0(i) = 2 * i;
-//   }
-//   bayesmix::Vector mu0_proto;
-//   bayesmix::to_proto(mu0, &mu0_proto);
-//   auto Lambda0 = Eigen::MatrixXd::Identity(dim, dim);
-//   bayesmix::Matrix Lambda0_proto;
-//   bayesmix::to_proto(Lambda0, &Lambda0_proto);
-//   double alpha0 = 2.0;
-//   double beta0 = 2.0;
-//   // Set parameters
-//   *prior.mutable_fixed_values()->mutable_mean() = mu0_proto;
-//   *prior.mutable_fixed_values()->mutable_var_scaling() = Lambda0_proto;
-//   prior.mutable_fixed_values()->set_shape(alpha0);
-//   prior.mutable_fixed_values()->set_scale(beta0);
-//   // Initialize hierarchy
-//   hier.get_mutable_prior()->CopyFrom(prior);
-//   hier.initialize();
+  // Create parameters, both Eigen and proto
+  Eigen::VectorXd mu0(dim);
+  for (int i = 0; i < dim; i++) {
+    mu0(i) = 2 * i;
+  }
+  bayesmix::Vector mu0_proto;
+  bayesmix::to_proto(mu0, &mu0_proto);
+  auto Lambda0 = Eigen::MatrixXd::Identity(dim, dim);
+  bayesmix::Matrix Lambda0_proto;
+  bayesmix::to_proto(Lambda0, &Lambda0_proto);
+  double alpha0 = 2.0;
+  double beta0 = 2.0;
+  // Set parameters
+  *prior.mutable_fixed_values()->mutable_mean() = mu0_proto;
+  *prior.mutable_fixed_values()->mutable_var_scaling() = Lambda0_proto;
+  prior.mutable_fixed_values()->set_shape(alpha0);
+  prior.mutable_fixed_values()->set_scale(beta0);
+  // Initialize hierarchy
+  hier.get_mutable_prior()->CopyFrom(prior);
+  hier.initialize();
 
-//   // Compute prior parameters
-//   Eigen::VectorXd mean = mu0;
-//   double var = beta0 / (alpha0 + 1);
+  // Compute prior parameters
+  Eigen::VectorXd mean = mu0;
+  double var = beta0 / (alpha0 + 1);
 
-//   // Compute posterior parameters
-//   Eigen::MatrixXd Lambda_n = Lambda0 + cov * cov.transpose();
-//   Eigen::VectorXd mu_n =
-//       stan::math::inverse_spd(Lambda_n) * (datum(0) * cov + Lambda0 * mu0);
-//   double alpha_n = alpha0 + 0.5;
-//   double beta_n =
-//       beta0 + 0.5 * (datum(0) * datum(0) + mu0.transpose() * Lambda0 * mu0 -
-//                      mu_n.transpose() * Lambda_n * mu_n);
-//   // Compute pieces
-//   double prior1 = stan::math::inv_gamma_lpdf(var, alpha0, beta0);
-//   double prior2 = stan::math::multi_normal_prec_lpdf(mean, mu0, Lambda0 /
-//   var); double pr = prior1 + prior2; double like = hier.get_like_lpdf(datum,
-//   cov); double post1 = stan::math::inv_gamma_lpdf(var, alpha_n, beta_n);
-//   double post2 =
-//       stan::math::multi_normal_prec_lpdf(mean, mu_n, Lambda_n / var);
-//   double post = post1 + post2;
+  // Compute posterior parameters
+  Eigen::MatrixXd Lambda_n = Lambda0 + cov * cov.transpose();
+  Eigen::VectorXd mu_n =
+      stan::math::inverse_spd(Lambda_n) * (datum(0) * cov + Lambda0 * mu0);
+  double alpha_n = alpha0 + 0.5;
+  double beta_n =
+      beta0 + 0.5 * (datum(0) * datum(0) + mu0.transpose() * Lambda0 * mu0 -
+                     mu_n.transpose() * Lambda_n * mu_n);
+  // Compute pieces
+  double prior1 = stan::math::inv_gamma_lpdf(var, alpha0, beta0);
+  double prior2 = stan::math::multi_normal_prec_lpdf(mean, mu0, Lambda0 / var);
+  double pr = prior1 + prior2;
+  double like = hier.get_like_lpdf(datum, cov);
+  double post1 = stan::math::inv_gamma_lpdf(var, alpha_n, beta_n);
+  double post2 =
+      stan::math::multi_normal_prec_lpdf(mean, mu_n, Lambda_n / var);
+  double post = post1 + post2;
 
-//   // Bayes: logmarg(x) = logprior(phi) + loglik(x|phi) - logpost(phi|x)
-//   double sum = pr + like - post;
-//   double marg = hier.prior_pred_lpdf(datum, cov);
+  // Bayes: logmarg(x) = logprior(phi) + loglik(x|phi) - logpost(phi|x)
+  double sum = pr + like - post;
+  double marg = hier.prior_pred_lpdf(datum, cov);
 
-//   ASSERT_FLOAT_EQ(sum, marg);
-// }
+  ASSERT_FLOAT_EQ(sum, marg);
+}
