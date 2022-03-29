977d0171b71b8beea76c8e6576086fa835a3a9bb
Matteo Gianella <gianeteo@hotmail.it>
2022-03-29 18:19:41 +0200

    Comments on SFINAE exception handling

--- src/hierarchies/priors/prior_model_internal.h
+++ src/hierarchies/priors/prior_model_internal.h
@@ -1,6 +1,14 @@
 #ifndef BAYESMIX_HIERARCHIES_PRIORS_PRIOR_MODEL_INTERNAL_H_
 #define BAYESMIX_HIERARCHIES_PRIORS_PRIOR_MODEL_INTERNAL_H_
 
+//! These functions exploit SFINAE to manage exception handling in all methods
+//! required only if end user wants to rely on Metropolis-like updaters. SFINAE
+//! (Substitution Failure Is Not An Error) is a C++ rule that applies during
+//! overload resolution of function templates: When substituting the explicitly
+//! specified or deduced type for the template parameter fails, the
+//! specialization is discarded from the overload set instead of causing a
+//! compile error. This feature is used in template metaprogramming.
+
 namespace internal {
 
 template <class Prior, typename T>
