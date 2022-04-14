5122f6b472fa23b42ef9db78d37d60129f017d51
Matteo Gianella <gianeteo@hotmail.it>
2022-04-14 12:03:19 +0200

    Improved exception handling

--- R/bayesmixr/R/build_bayesmix.R
+++ R/bayesmixr/R/build_bayesmix.R
@@ -6,7 +6,7 @@
 #' @param nproc Number of processes to use for parallel compilation. Thanks to \code{parallel} package,
 #' this parameter defaults to half of the available processes (through \code{\link[parallel]{detectCores}} function)
 #' @param build_dirname Name for the build directory of BayesMix. Default is "build".
-#' @return \code{TRUE} if build is successfull, \code{FALSE} otherwise
+#' @return \code{TRUE} if build is successful,it raises errors otherwise
 #'
 #' @export
 build_bayesmix <- function(nproc = ceiling(parallel::detectCores()/2), build_dirname = "build") {
@@ -22,42 +22,22 @@ build_bayesmix <- function(nproc = ceiling(parallel::detectCores()/2), build_dir
   # Build bayesmix
   build_dir = sprintf("%s/%s", bayesmix_home, build_dirname)
   flags = "-DDISABLE_DOCS=TRUE -DDISABLE_BENCHMARKS=TRUE -DDISABLE_TESTS=TRUE -DDISABLE_PLOTS=TRUE -DCMAKE_BUILD_TYPE=Release"
-  cat("Configuring bayesmix ... ")
+  cat("*** Configuring BayesMix ***\n")
   CONFIGURE = sprintf("mkdir -p %s && cd %s && cmake .. %s", build_dir, build_dir, flags)
-  tryCatch({
-    system(CONFIGURE, ignore.stdout = TRUE, ignore.stderr = TRUE)
-    cat("DONE ✔ \n")
-    },
-    warning = function(w){
-      message(sprintf("\nWarning during configure: %s\n", as.character(w)))
-    },
-    error = function(e) {
-      message(sprintf("\nConfigure failed with error: %s\n)", as.character(e)))
-      return(FALSE)
-    })
+  errlog = system(CONFIGURE, ignore.stdout = TRUE, ignore.stderr = TRUE)
+  if(errlog != 0) { stop(sprintf("Something went wrong during configure: exit with status %d", errlog)) }
 
+  # Make run_mcmc executable
+  cat("*** Building BayesMix executable ***\n")
   BUILD = sprintf("cd %s && make run_mcmc -j%d", build_dir, nproc)
-  tryCatch({
-    # Make run_mcmc executable
-    cat("Building bayesmix executable:\n")
-    system(BUILD)
+  errlog = system(BUILD)
+  if(errlog != 0) { stop(sprintf("Something went wrong during build: exit with status %d", errlog)) }
 
-    # Set BAYESMIX_EXE environment variable for R
-    renviron = system.file(".Renviron", package = "bayesmixr")
-    if(Sys.getenv("BAYESMIX_EXE") == ""){
-      cat("Setting BAYESMIX_EXE environment variable ... ")
-      write(x = sprintf("BAYESMIX_EXE=%s/run_mcmc", build_dir), file = renviron, append = TRUE)
-      readRenviron(renviron)
-      cat("DONE ✔ \n")
-    }
-    cat("Successfully installed bayesmix.\n")
-    return(TRUE)
-  },
-  warning = function(w){
-    message(sprintf("\nWarning during build: %s\n", as.character(w)))
-  },
-  error = function(e){
-    message(sprintf("\nBuild failed with error: %s\n)", as.character(e)))
-    return(FALSE)
-  })
+  # Set BAYESMIX_EXE environment variable
+  cat("*** Setting BAYESMIX_EXE environment variable ***\n")
+  renviron = system.file(".Renviron", package = "bayesmixr")
+  write(x = sprintf("BAYESMIX_EXE=%s/run_mcmc", build_dir), file = renviron, append = TRUE)
+  readRenviron(renviron)
+  cat("Successfully installed BayesMix\n")
+  return(TRUE)
 }
