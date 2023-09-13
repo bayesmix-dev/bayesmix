70d1e47516ba6f10e1a76c36ecfef0305216cdd5
TeoGiane <gianeteo@hotmail.it>
2023-09-13 14:00:32 +0200

    Improved unit tests with new API

--- R/bayesmixr/tests/testthat/test_build.R
+++ R/bayesmixr/tests/testthat/test_build.R
@@ -1,4 +1,4 @@
 test_that("build_bayesmix() is successful", {
   cat("\n")
-  testthat::expect_true(build_bayesmix())
+  testthat::expect_no_error(build_bayesmix())
 })
--- R/bayesmixr/tests/testthat/test_run.R
+++ R/bayesmixr/tests/testthat/test_run.R
@@ -32,7 +32,7 @@ test_that("run_mcmc() is successful", {
   # 1st bunch of tests
   cat("\n")
   out = run_mcmc("NNIG", "DP", data, GO_PARAMS, DP_PARAMS, ALGO_PARAMS, grid,
-                 return_clusters=FALSE, return_num_clusters=FALSE, return_best_clus=FALSE)
+                 return_clusters=FALSE, return_num_clusters=FALSE, return_best_clus=FALSE, return_chains = FALSE)
   testthat::expect_type(out$eval_dens, "double")
   testthat::expect_equal(dim(out$eval_dens)[1], 5)
   testthat::expect_equal(dim(out$eval_dens)[2], length(grid))
@@ -43,17 +43,7 @@ test_that("run_mcmc() is successful", {
   # 2nd bunch of tests
   cat("\n")
   out = run_mcmc("NNIG", "DP", data, GO_PARAMS, DP_PARAMS, ALGO_PARAMS, NULL,
-                 return_clusters=FALSE, return_num_clusters=TRUE, return_best_clus=FALSE)
-  testthat::expect_null(out$eval_dens)
-  testthat::expect_type(out$n_clus, "integer")
-  testthat::expect_equal(length(out$n_clus), 5)
-  testthat::expect_null(out$clus)
-  testthat::expect_null(out$best_clus)
-
-  # 3rd bunch of tests
-  cat("\n")
-  out = run_mcmc("NNIG", "DP", data, GO_PARAMS, DP_PARAMS, ALGO_PARAMS, NULL,
-                 return_clusters=TRUE, return_num_clusters=FALSE, return_best_clus=FALSE)
+                 return_clusters=TRUE, return_num_clusters=FALSE, return_best_clus=FALSE, return_chains = FALSE)
   testthat::expect_null(out$eval_dens)
   testthat::expect_null(out$n_clus)
   testthat::expect_type(out$clus, "integer")
@@ -61,13 +51,37 @@ test_that("run_mcmc() is successful", {
   testthat::expect_equal(dim(out$clus)[2], length(data))
   testthat::expect_null(out$best_clus)
 
+  # 3rd bunch of tests
+  cat("\n")
+  out = run_mcmc("NNIG", "DP", data, GO_PARAMS, DP_PARAMS, ALGO_PARAMS, NULL,
+                 return_clusters=FALSE, return_num_clusters=TRUE, return_best_clus=FALSE, return_chains = FALSE)
+  testthat::expect_null(out$eval_dens)
+  testthat::expect_type(out$n_clus, "integer")
+  testthat::expect_equal(length(out$n_clus), 5)
+  testthat::expect_null(out$clus)
+  testthat::expect_null(out$best_clus)
+
   # 4th bunch of tests
   cat("\n")
   out = run_mcmc("NNIG", "DP", data, GO_PARAMS, DP_PARAMS, ALGO_PARAMS, NULL,
-                 return_clusters=FALSE, return_num_clusters=FALSE, return_best_clus=TRUE)
+                 return_clusters=FALSE, return_num_clusters=FALSE, return_best_clus=TRUE, return_chains = FALSE)
   testthat::expect_null(out$eval_dens)
   testthat::expect_null(out$n_clus)
   testthat::expect_null(out$clus)
   testthat::expect_type(out$best_clus, "integer")
   testthat::expect_equal(length(out$best_clus), length(data))
+
+  # 5th bunch of tests
+  cat("\n")
+  out = run_mcmc("NNIG", "DP", data, GO_PARAMS, DP_PARAMS, ALGO_PARAMS, NULL,
+                 return_clusters=FALSE, return_num_clusters=FALSE, return_best_clus=FALSE, return_chains = TRUE)
+  testthat::expect_null(out$eval_dens)
+  testthat::expect_null(out$n_clus)
+  testthat::expect_null(out$clus)
+  testthat::expect_null(out$best_clus)
+  testthat::expect_type(out$mcmc_chains, "list")
+  testthat::expect_equal(length(out$mcmc_chains), 5)
+  testthat::expect_true(all(sapply(out$mcmc_chains, function(x){is(x, "Message")})))
+  testthat::expect_true(all(sapply(out$mcmc_chains, function(x){x$descriptor()@type == "bayesmix.AlgorithmState"})))
+  testthat::expect_true(all(sapply(out$mcmc_chains, function(x){x$size("cluster_allocs") == 10})))
 })
