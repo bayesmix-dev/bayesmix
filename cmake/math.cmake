c26667416dec7d94f99dbc27138e7f7f0002f43f
TeoGiane <gianeteo@hotmail.it>
2023-09-19 10:12:46 +0200

    stan::math installation now is Windows compatible

--- cmake/math.cmake
+++ cmake/math.cmake
@@ -1,26 +1,63 @@
+# Define patch command to inject
+set(math-tbb_patch git apply ${BASEPATH}/resources/patches/math-tbb.patch)
+
 # Fetching bayesmix-dev/math
 message(STATUS "")
 message(STATUS "Fetching bayesmix-dev/math")
 FetchContent_Declare(math
   GIT_REPOSITORY "https://github.com/bayesmix-dev/math.git"
   GIT_TAG "develop"
-  OVERRIDE_FIND_PACKAGE
+  PATCH_COMMAND ${math-tbb_patch}
 )
 FetchContent_MakeAvailable(math)
 
-# Build bayesmix-dev/math TBB library 
-message(STATUS "Build bayesmix-dev/math TBB")
-
-# Define TBB_ROOT Folder
+# Set TBB_ROOT variable
 set(TBB_ROOT ${math_SOURCE_DIR}/lib/tbb)
-file(COPY ${math_SOURCE_DIR}/lib/tbb_2020.3/ DESTINATION ${TBB_ROOT})
 
-# Build TBB Library with CMake Integration
-include(${TBB_ROOT}/cmake/TBBBuild.cmake)
-list(APPEND MAKE_ARGS "tbb_build_dir=${TBB_ROOT}")
-list(APPEND MAKE_ARGS "tbb_build_prefix=tbb")
-tbb_build(TBB_ROOT ${TBB_ROOT} CONFIG_DIR TBB_DIR MAKE_ARGS ${MAKE_ARGS})
+# Define make command
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+  set(MAKE_COMMAND mingw32-make)
+else()
+  set(MAKE_COMMAND make)
+endif()
 
-# Find math and TBB packages
-find_package(math REQUIRED)
-find_package(TBB REQUIRED)
+# Set compiler flags
+file(APPEND ${math_SOURCE_DIR}/make/local "TBB_CXX_TYPE=gcc\n")
+
+# Set extra compiler flags for Windows
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+  file(APPEND ${math_SOURCE_DIR}/make/local "CXXFLAGS+=-Wno-nonnull\n")
+  file(APPEND ${math_SOURCE_DIR}/make/local "TBB_CXXFLAGS=-U__MSVCRT_VERSION__ -D__MSVCRT_VERSION__=0x0E00\n")
+endif()
+
+# Compile math libraries
+message(STATUS "Compiling math libraries ...")
+execute_process(
+  COMMAND ${MAKE_COMMAND} -f ./make/standalone math-libs
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${math_SOURCE_DIR}
+)
+if(result)
+  message(FATAL_ERROR "Failed to compile math libraries (${result})!")
+endif()
+
+# Add TBB link directory
+link_directories(${TBB_ROOT})
+
+# In Windows, add TBB_ROOT to PATH variable via batch file if not present
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+  # Check if adding TBB_ROOT is already present in PATH
+  file(TO_CMAKE_PATH "$ENV{PATH}" PATH)
+  string(FIND "${PATH}" "${TBB_ROOT}" tbb_path-LOCATION)
+  # If not present, add to PATH user environment variable
+  if(tbb_path-LOCATION EQUAL -1)
+    execute_process(
+      COMMAND cmd.exe /C install-tbb.bat
+      RESULT_VARIABLE result
+      WORKING_DIRECTORY ${BASEPATH}
+    )
+    if(result)
+      message(FATAL_ERROR "Failed to install TBB (${result})!")
+    endif()
+  endif()
+endif()
---
+++ install-tbb.bat
@@ -0,0 +1,5 @@
+@ECHO off
+echo Permanently setting TBB_ROOT to the PATH user environment variable:
+
+for /F "tokens=2* delims= " %%f IN ('reg query HKCU\Environment /v PATH ^| findstr /i path') do set OLD_SYSTEM_PATH=%%g
+setx Path %~dp0lib\_deps\math-src\lib\tbb;%OLD_SYSTEM_PATH%
